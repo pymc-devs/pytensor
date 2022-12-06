571b106f5ac414c00c0d5eb070b067d20fcbb130
Rémi Louf <remilouf@gmail.com>
2022-12-06 17:55:18 +0100

    Rewrite `size` input of `RandomVariable`s in JAX backend

--- pytensor/tensor/random/rewriting/__init__.py
+++ pytensor/tensor/random/rewriting/__init__.py
@@ -1,2 +1,10 @@
 # TODO: This is for backward-compatibility; remove when reasonable.
 from pytensor.tensor.random.rewriting.basic import *
+
+
+# isort: off
+
+# Register JAX specializations
+import pytensor.tensor.random.rewriting.jax
+
+# isort: on
