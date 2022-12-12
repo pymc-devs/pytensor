fc2dc234a06b0f52736fc35529a81be5f41e29a3
Maxim Kochurov <max.kochurov@pymc-devs.org>
2022-12-12 17:30:08 +0300

    remove deprecated modules from pytensor.graph

--- pytensor/graph/toolbox.py
+++
@@ -1,9 +0,0 @@
-import warnings
-
-
-warnings.warn(
-    "The module `pytensor.graph.toolbox` is deprecated "
-    "and has been renamed to `pytensor.graph.features`",
-    DeprecationWarning,
-    stacklevel=2,
-)
