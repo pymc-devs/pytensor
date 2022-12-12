119be16ced95274c6af23e2481c1744141513849
Rémi Louf <remilouf@gmail.com>
2022-12-12 14:42:01 +0100

    Add rewrites to re-express boolean indexing logic

--- pytensor/tensor/rewriting/__init__.py
+++ pytensor/tensor/rewriting/__init__.py
@@ -1,6 +1,9 @@
 import pytensor.tensor.rewriting.basic
 import pytensor.tensor.rewriting.elemwise
 import pytensor.tensor.rewriting.extra_ops
+
+# Register JAX specializations
+import pytensor.tensor.rewriting.jax
 import pytensor.tensor.rewriting.math
 import pytensor.tensor.rewriting.shape
 import pytensor.tensor.rewriting.special
