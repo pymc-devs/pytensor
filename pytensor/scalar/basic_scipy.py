7d173623817ae27adb310834f068b0ee77cc725c
Maxim Kochurov <max.kochurov@pymc-devs.org>
2022-12-14 00:09:49 +0300

    remove deprecated pytensor.tensor.nnet

--- pytensor/scalar/basic_scipy.py
+++
@@ -1,9 +0,0 @@
-import warnings
-
-
-warnings.warn(
-    "The module `pytensor.scalar.basic_scipy` is deprecated "
-    "and has been renamed to `pytensor.scalar.math`",
-    DeprecationWarning,
-    stacklevel=2,
-)
