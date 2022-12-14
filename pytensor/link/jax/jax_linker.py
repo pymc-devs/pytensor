0b92f87489c12b6d0f38e4794c0424810284bd0f
Maxim Kochurov <max.kochurov@pymc-devs.org>
2022-12-14 15:22:05 +0300

    remove deprecated module pytensor/link/jax/jax_linker.py

--- pytensor/link/jax/jax_linker.py
+++
@@ -1,9 +0,0 @@
-import warnings
-
-
-warnings.warn(
-    "The module `pytensor.link.jax.jax_linker` is deprecated "
-    "and has been renamed to `pytensor.link.jax.linker`",
-    DeprecationWarning,
-    stacklevel=2,
-)
