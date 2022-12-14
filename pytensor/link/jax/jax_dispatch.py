f6ec71b258c68deae8caff8641c845f95dc14f72
Maxim Kochurov <max.kochurov@pymc-devs.org>
2022-12-14 15:21:26 +0300

    remove deprecated module pytensor/link/jax/jax_dispatch.py

--- pytensor/link/jax/jax_dispatch.py
+++
@@ -1,9 +0,0 @@
-import warnings
-
-
-warnings.warn(
-    "The module `pytensor.link.jax.jax_dispatch` is deprecated "
-    "and has been renamed to `pytensor.link.jax.dispatch`",
-    DeprecationWarning,
-    stacklevel=2,
-)
