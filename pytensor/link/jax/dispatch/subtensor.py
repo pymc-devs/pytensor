aesara-devs/aesara

Raise when trying to slice with a dynamic length

aesara/link/jax/dispatch/subtensor.py
@@ -28,31 +28,43 @@ can be re-expressed as:
 >>> y_at = at.where(x_at > 0, x_at, 0).sum()
 """
 
+DYNAMIC_SLICE_LENGTH_ERROR = """JAX does not support slicing arrays with a dynamic
+slice length.
+"""
 
-def assert_indices_jax_compatible(node):
-    ilist = node.inputs[1]
-    if ilist.type.dtype == "bool":
-        raise NotImplementedError(BOOLEAN_MASK_ERROR)
+
+def assert_indices_jax_compatible(node, idx_list):
+    from aesara.graph.basic import Constant
+    from aesara.tensor.var import TensorVariable
+
+    ilist = indices_from_subtensor(node.inputs[1:], idx_list)
+    for idx in ilist:
+
+        if isinstance(idx, TensorVariable):
+            if idx.type.dtype == "bool":
+                raise NotImplementedError(BOOLEAN_MASK_ERROR)
+        elif isinstance(idx, slice):
+            for slice_arg in (idx.start, idx.stop, idx.step):
+                if slice_arg is not None and not isinstance(slice_arg, Constant):
+                    raise NotImplementedError(DYNAMIC_SLICE_LENGTH_ERROR)
 
 
 @jax_funcify.register(Subtensor)
 @jax_funcify.register(AdvancedSubtensor)
 @jax_funcify.register(AdvancedSubtensor1)
 def jax_funcify_Subtensor(op, node, **kwargs):
-    assert_indices_jax_compatible(node)
 
     idx_list = getattr(op, "idx_list", None)
+    assert_indices_jax_compatible(node, idx_list)
 
-    def subtensor(x, *ilists):
-
+    def subtensor_constant(x, *ilists):
         indices = indices_from_subtensor(ilists, idx_list)
-
         if len(indices) == 1:
             indices = indices[0]
 
         return x.__getitem__(indices)
 
-    return subtensor
+    return subtensor_constant
 
 
 @jax_funcify.register(IncSubtensor)

pymc-devs/pytensor

pytensor/link/jax/dispatch/subtensor.py
@@ -28,31 +28,43 @@ can be re-expressed as:
 >>> y_at = at.where(x_at > 0, x_at, 0).sum()
 """
 
+DYNAMIC_SLICE_LENGTH_ERROR = """JAX does not support slicing arrays with a dynamic
+slice length.
+"""
 
-def assert_indices_jax_compatible(node):
-    ilist = node.inputs[1]
-    if ilist.type.dtype == "bool":
-        raise NotImplementedError(BOOLEAN_MASK_ERROR)
+
+def assert_indices_jax_compatible(node, idx_list):
+    from pytensor.graph.basic import Constant
+    from pytensor.tensor.var import TensorVariable
+
+    ilist = indices_from_subtensor(node.inputs[1:], idx_list)
+    for idx in ilist:
+
+        if isinstance(idx, TensorVariable):
+            if idx.type.dtype == "bool":
+                raise NotImplementedError(BOOLEAN_MASK_ERROR)
+        elif isinstance(idx, slice):
+            for slice_arg in (idx.start, idx.stop, idx.step):
+                if slice_arg is not None and not isinstance(slice_arg, Constant):
+                    raise NotImplementedError(DYNAMIC_SLICE_LENGTH_ERROR)
 
 
 @jax_funcify.register(Subtensor)
 @jax_funcify.register(AdvancedSubtensor)
 @jax_funcify.register(AdvancedSubtensor1)
 def jax_funcify_Subtensor(op, node, **kwargs):
-    assert_indices_jax_compatible(node)
 
     idx_list = getattr(op, "idx_list", None)
+    assert_indices_jax_compatible(node, idx_list)
 
-    def subtensor(x, *ilists):
-
+    def subtensor_constant(x, *ilists):
         indices = indices_from_subtensor(ilists, idx_list)
-
         if len(indices) == 1:
             indices = indices[0]
 
         return x.__getitem__(indices)
 
-    return subtensor
+    return subtensor_constant
 
 
 @jax_funcify.register(IncSubtensor)