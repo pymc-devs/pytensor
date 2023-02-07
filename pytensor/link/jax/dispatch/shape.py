aesara-devs/aesara

Refactor the JAX implementation of `Reshape`

aesara/link/jax/dispatch/shape.py
@@ -28,11 +28,38 @@ def jax_funcify_JAXShapeTuple(op, **kwargs):
     return shape_tuple_fn
 
 
+SHAPE_NOT_COMPATIBLE = """JAX requires concrete values for the `shape` parameter of `jax.numpy.reshape`.
+Concrete values are either constants:
+
+>>> import aesara.tensor as at
+>>> x = at.ones(6)
+>>> y = x.reshape((2, 3))
+
+Or the shape of an array:
+
+>>> mat = at.matrix('mat')
+>>> y = x.reshape(mat.shape)
+"""
+
+
+def assert_shape_argument_jax_compatible(shape):
+    """Assert whether the current node can be JIT-compiled by JAX.
+
+    JAX can JIT-compile functions with a `shape` or `size` argument if it is
+    given a concrete value, i.e. either a constant or the shape of any traced
+    value.
+
+    """
+    shape_op = shape.owner.op
+    if not isinstance(shape_op, (Shape, Shape_i, JAXShapeTuple)):
+        raise NotImplementedError(SHAPE_NOT_COMPATIBLE)
+
+
 @jax_funcify.register(Reshape)
 def jax_funcify_Reshape(op, node, **kwargs):
 
-    # JAX reshape only works with constant inputs, otherwise JIT fails
     shape = node.inputs[1]
+
     if isinstance(shape, Constant):
         constant_shape = shape.data
 
@@ -40,6 +67,7 @@ def jax_funcify_Reshape(op, node, **kwargs):
             return jnp.reshape(x, constant_shape)
 
     else:
+        assert_shape_argument_jax_compatible(shape)
 
         def reshape(x, shape):
             return jnp.reshape(x, shape)

aesara/tensor/rewriting/jax.py
@@ -1,9 +1,12 @@
+import aesara.tensor as at
 from aesara.compile import optdb
 from aesara.graph.rewriting.basic import in2out, node_rewriter
-from aesara.tensor.var import TensorVariable
-import aesara.tensor as at
-from aesara.tensor.subtensor import AdvancedIncSubtensor, AdvancedSubtensor
+from aesara.tensor.basic import MakeVector
+from aesara.tensor.elemwise import DimShuffle
 from aesara.tensor.math import Sum
+from aesara.tensor.shape import Reshape
+from aesara.tensor.subtensor import AdvancedIncSubtensor, AdvancedSubtensor
+from aesara.tensor.var import TensorVariable
 
 
 @node_rewriter([AdvancedIncSubtensor])
@@ -24,7 +27,7 @@ def boolean_indexing_set_or_inc(fgraph, node):
     if not isinstance(cond, TensorVariable):
         return
 
-    if not cond.type.dtype == 'bool':
+    if not cond.type.dtype == "bool":
         return
 
     if op.set_instead_of_inc:
@@ -36,7 +39,10 @@ def boolean_indexing_set_or_inc(fgraph, node):
 
 
 optdb.register(
-    "jax_boolean_indexing_set_or_inc", in2out(boolean_indexing_set_or_inc), "jax", position=100
+    "jax_boolean_indexing_set_or_inc",
+    in2out(boolean_indexing_set_or_inc),
+    "jax",
+    position=100,
 )
 
 
@@ -67,12 +73,63 @@ def boolean_indexing_sum(fgraph, node):
     if not isinstance(cond, TensorVariable):
         return
 
-    if not cond.type.dtype == 'bool':
+    if not cond.type.dtype == "bool":
         return
 
     out = at.sum(at.where(cond, x, 0))
     return out.owner.outputs
 
+
 optdb.register(
     "jax_boolean_indexing_sum", in2out(boolean_indexing_sum), "jax", position=100
+)
+
+
+@node_rewriter([Reshape])
+def shape_parameter_as_tuple(fgraph, node):
+    """Replace `MakeVector` and `DimShuffle` (when used to transform a scalar
+    into a 1d vector) when they are found as the input of a `shape`
+    parameter by `JAXShapeTuple` during transpilation.
+
+    The JAX implementations of `MakeVector` and `DimShuffle` always return JAX
+    `TracedArrays`, but JAX only accepts concrete values as inputs for the `size`
+    or `shape` parameter. When these `Op`s are used to convert scalar or tuple
+    inputs, however, we can avoid tracing by making them return a tuple of their
+    inputs instead.
+
+    Note that JAX does not accept scalar inputs for the `size` or `shape`
+    parameters, and this rewrite also ensures that scalar inputs are turned into
+    tuples during transpilation.
+
+    """
+    from aesara.link.jax.dispatch.shape import JAXShapeTuple
+
+    shape_arg = node.inputs[1]
+    shape_node = shape_arg.owner
+
+    if shape_node is None:
+        return
+
+    if isinstance(shape_node.op, JAXShapeTuple):
+        return
+
+    if isinstance(shape_node.op, MakeVector) or (
+        isinstance(shape_node.op, DimShuffle)
+        and shape_node.op.input_broadcastable == ()
+        and shape_node.op.new_order == ("x",)
+    ):
+        # Here Aesara converted a tuple or list to a tensor
+        new_shape_args = JAXShapeTuple()(*shape_node.inputs)
+        new_inputs = list(node.inputs)
+        new_inputs[1] = new_shape_args
+
+        new_node = node.clone_with_new_inputs(new_inputs)
+        return new_node.outputs
+
+
+optdb.register(
+    "jax_shape_parameter_as_tuple",
+    in2out(shape_parameter_as_tuple),
+    "jax",
+    position=100,
 )

pymc-devs/pytensor

pytensor/link/jax/dispatch/shape.py
@@ -28,11 +28,38 @@ def jax_funcify_JAXShapeTuple(op, **kwargs):
     return shape_tuple_fn
 
 
+SHAPE_NOT_COMPATIBLE = """JAX requires concrete values for the `shape` parameter of `jax.numpy.reshape`.
+Concrete values are either constants:
+
+>>> import pytensor.tensor as at
+>>> x = at.ones(6)
+>>> y = x.reshape((2, 3))
+
+Or the shape of an array:
+
+>>> mat = at.matrix('mat')
+>>> y = x.reshape(mat.shape)
+"""
+
+
+def assert_shape_argument_jax_compatible(shape):
+    """Assert whether the current node can be JIT-compiled by JAX.
+
+    JAX can JIT-compile functions with a `shape` or `size` argument if it is
+    given a concrete value, i.e. either a constant or the shape of any traced
+    value.
+
+    """
+    shape_op = shape.owner.op
+    if not isinstance(shape_op, (Shape, Shape_i, JAXShapeTuple)):
+        raise NotImplementedError(SHAPE_NOT_COMPATIBLE)
+
+
 @jax_funcify.register(Reshape)
 def jax_funcify_Reshape(op, node, **kwargs):
 
-    # JAX reshape only works with constant inputs, otherwise JIT fails
     shape = node.inputs[1]
+
     if isinstance(shape, Constant):
         constant_shape = shape.data
 
@@ -40,6 +67,7 @@ def jax_funcify_Reshape(op, node, **kwargs):
             return jnp.reshape(x, constant_shape)
 
     else:
+        assert_shape_argument_jax_compatible(shape)
 
         def reshape(x, shape):
             return jnp.reshape(x, shape)

pytensor/tensor/rewriting/jax.py
@@ -1,9 +1,12 @@
+import pytensor.tensor as at
 from pytensor.compile import optdb
 from pytensor.graph.rewriting.basic import in2out, node_rewriter
-from pytensor.tensor.var import TensorVariable
-import pytensor.tensor as at
-from pytensor.tensor.subtensor import AdvancedIncSubtensor, AdvancedSubtensor
+from pytensor.tensor.basic import MakeVector
+from pytensor.tensor.elemwise import DimShuffle
 from pytensor.tensor.math import Sum
+from pytensor.tensor.shape import Reshape
+from pytensor.tensor.subtensor import AdvancedIncSubtensor, AdvancedSubtensor
+from pytensor.tensor.var import TensorVariable
 
 
 @node_rewriter([AdvancedIncSubtensor])
@@ -24,7 +27,7 @@ def boolean_indexing_set_or_inc(fgraph, node):
     if not isinstance(cond, TensorVariable):
         return
 
-    if not cond.type.dtype == 'bool':
+    if not cond.type.dtype == "bool":
         return
 
     if op.set_instead_of_inc:
@@ -36,7 +39,10 @@ def boolean_indexing_set_or_inc(fgraph, node):
 
 
 optdb.register(
-    "jax_boolean_indexing_set_or_inc", in2out(boolean_indexing_set_or_inc), "jax", position=100
+    "jax_boolean_indexing_set_or_inc",
+    in2out(boolean_indexing_set_or_inc),
+    "jax",
+    position=100,
 )
 
 
@@ -67,12 +73,63 @@ def boolean_indexing_sum(fgraph, node):
     if not isinstance(cond, TensorVariable):
         return
 
-    if not cond.type.dtype == 'bool':
+    if not cond.type.dtype == "bool":
         return
 
     out = at.sum(at.where(cond, x, 0))
     return out.owner.outputs
 
+
 optdb.register(
     "jax_boolean_indexing_sum", in2out(boolean_indexing_sum), "jax", position=100
+)
+
+
+@node_rewriter([Reshape])
+def shape_parameter_as_tuple(fgraph, node):
+    """Replace `MakeVector` and `DimShuffle` (when used to transform a scalar
+    into a 1d vector) when they are found as the input of a `shape`
+    parameter by `JAXShapeTuple` during transpilation.
+
+    The JAX implementations of `MakeVector` and `DimShuffle` always return JAX
+    `TracedArrays`, but JAX only accepts concrete values as inputs for the `size`
+    or `shape` parameter. When these `Op`s are used to convert scalar or tuple
+    inputs, however, we can avoid tracing by making them return a tuple of their
+    inputs instead.
+
+    Note that JAX does not accept scalar inputs for the `size` or `shape`
+    parameters, and this rewrite also ensures that scalar inputs are turned into
+    tuples during transpilation.
+
+    """
+    from pytensor.link.jax.dispatch.shape import JAXShapeTuple
+
+    shape_arg = node.inputs[1]
+    shape_node = shape_arg.owner
+
+    if shape_node is None:
+        return
+
+    if isinstance(shape_node.op, JAXShapeTuple):
+        return
+
+    if isinstance(shape_node.op, MakeVector) or (
+        isinstance(shape_node.op, DimShuffle)
+        and shape_node.op.input_broadcastable == ()
+        and shape_node.op.new_order == ("x",)
+    ):
+        # Here PyTensor converted a tuple or list to a tensor
+        new_shape_args = JAXShapeTuple()(*shape_node.inputs)
+        new_inputs = list(node.inputs)
+        new_inputs[1] = new_shape_args
+
+        new_node = node.clone_with_new_inputs(new_inputs)
+        return new_node.outputs
+
+
+optdb.register(
+    "jax_shape_parameter_as_tuple",
+    in2out(shape_parameter_as_tuple),
+    "jax",
+    position=100,
 )