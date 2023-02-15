aesara-devs/aesara

replaced true_div with true_divide

aesara/tensor/rewriting/special.py
@@ -3,7 +3,7 @@ from aesara.graph.rewriting.basic import copy_stack_trace, node_rewriter
 from aesara.tensor.elemwise import DimShuffle, Elemwise
 from aesara.tensor.math import Sum, exp
 from aesara.tensor.math import sum as at_sum
-from aesara.tensor.math import true_div
+from aesara.tensor.math import true_divide
 from aesara.tensor.rewriting.basic import register_specialize
 from aesara.tensor.rewriting.math import local_mul_canonizer
 from aesara.tensor.special import LogSoftmax, Softmax, SoftmaxGrad
@@ -50,7 +50,7 @@ def local_logsoftmax_grad(fgraph, node):
         isinstance(node.op, SoftmaxGrad)
         and len(node.inputs) == 2
         and node.inputs[0].owner is not None
-        and node.inputs[0].owner.op == true_div
+        and node.inputs[0].owner.op == true_divide
         and len(node.inputs[0].owner.inputs) >= 2
         and node.inputs[0].owner.inputs[1].owner is not None
         and isinstance(node.inputs[0].owner.inputs[1].owner.op, Softmax)
@@ -58,7 +58,7 @@ def local_logsoftmax_grad(fgraph, node):
         and not (
             # skip if it will be optimized by
             # local_advanced_indexing_crossentropy_onehot_grad
-            node.inputs[0].owner.op == true_div
+            node.inputs[0].owner.op == true_divide
             and node.inputs[0].owner.inputs[0].owner is not None
             and isinstance(
                 node.inputs[0].owner.inputs[0].owner.op, AdvancedIncSubtensor

tests/tensor/rewriting/test_basic.py
@@ -52,7 +52,7 @@ from aesara.tensor.math import (
 from aesara.tensor.math import pow as at_pow
 from aesara.tensor.math import softplus, sqrt, sub
 from aesara.tensor.math import sum as at_sum
-from aesara.tensor.math import true_div
+from aesara.tensor.math import true_divide
 from aesara.tensor.rewriting.basic import (
     assert_op,
     local_alloc_sink_dimshuffle,
@@ -898,7 +898,7 @@ class TestLocalSwitchSink:
                 (dvector("x"), self.xv),
                 (dscalar("x"), self.xs),
             ]:
-                y = true_div(
+                y = true_divide(
                     at.switch(condition[0] > 0, 1.0 * x[0], 0.0 * x[0]),
                     at.switch(condition[0] > 0, 1.0 * x[0], log(c) * x[0]),
                 )
@@ -1083,7 +1083,7 @@ class TestLocalMergeSwitchSameCond:
             add,
             sub,
             mul,
-            true_div,
+            true_divide,
             int_div,
             floor_div,
             minimum,
@@ -1447,7 +1447,7 @@ def test_local_upcast_elemwise_constant_inputs():
     # This tests a corner case for which the rewrite should not be applied.
     with config.change_flags(floatX="float32"):
         v = lvector()
-        function([v], true_div(v, 2))
+        function([v], true_divide(v, 2))
 
 
 def test_assert_op_gradient():

tests/tensor/rewriting/test_special.py
@@ -9,7 +9,7 @@ from aesara.configdefaults import config
 from aesara.graph.fg import FunctionGraph
 from aesara.graph.rewriting.basic import check_stack_trace
 from aesara.graph.rewriting.db import RewriteDatabaseQuery
-from aesara.tensor.math import add, exp, log, true_div
+from aesara.tensor.math import add, exp, log, true_divide
 from aesara.tensor.special import LogSoftmax, Softmax, SoftmaxGrad, softmax
 from aesara.tensor.type import matrix
 from tests import unittest_tools as utt
@@ -78,7 +78,7 @@ class TestLogSoftmaxRewrites:
         softmax_grad_node = g.owner
         assert softmax_grad_node.op == SoftmaxGrad(axis=-1)
         true_div_node = softmax_grad_node.inputs[0].owner
-        assert true_div_node.op == true_div
+        assert true_div_node.op == true_divide
 
         # We replace thk elemwise true_div op by an elemwise add.
         new_g = SoftmaxGrad(axis=-1)(

tests/tensor/test_math.py
@@ -109,7 +109,7 @@ from aesara.tensor.math import (
     sub,
 )
 from aesara.tensor.math import sum as at_sum
-from aesara.tensor.math import tan, tanh, tensordot, true_div, trunc, var
+from aesara.tensor.math import tan, tanh, tensordot, true_divide, trunc, var
 from aesara.tensor.type import (
     TensorType,
     complex_dtypes,
@@ -327,7 +327,7 @@ _grad_broadcast_div_mod_normal = dict(
 # fmt: on
 
 TestTrueDivBroadcast = makeBroadcastTester(
-    op=true_div,
+    op=true_divide,
     expected=_numpy_true_div,
     good=_good_broadcast_div_mod_normal_float_no_complex,
     grad=_grad_broadcast_div_mod_normal,
@@ -1832,8 +1832,8 @@ class TestDivimpl:
         assert np.allclose(function([i, f], f / i)(5, 11.0), (11.0 / 5.0))
         assert np.allclose(function([i, ii], i // ii)(5, 3), (5 // 3))
         assert np.allclose(function([i, ii], ii // i)(5, 3), (3 // 5))
-        assert np.allclose(function([i, ii], true_div(i, ii))(5, 3), (5.0 / 3.0))
-        assert np.allclose(function([i, ii], true_div(ii, i))(5, 3), (3.0 / 5.0))
+        assert np.allclose(function([i, ii], true_divide(i, ii))(5, 3), (5.0 / 3.0))
+        assert np.allclose(function([i, ii], true_divide(ii, i))(5, 3), (3.0 / 5.0))
         assert np.allclose(function([i, c], i / c)(5, complex(5, 3)), (5.0 / (5 + 3j)))
         assert np.allclose(function([i, c], c / i)(5, complex(5, 3)), ((5 + 3j) / 5.0))
 

pymc-devs/pytensor

pytensor/tensor/rewriting/special.py
@@ -3,7 +3,7 @@ from pytensor.graph.rewriting.basic import copy_stack_trace, node_rewriter
 from pytensor.tensor.elemwise import DimShuffle, Elemwise
 from pytensor.tensor.math import Sum, exp
 from pytensor.tensor.math import sum as at_sum
-from pytensor.tensor.math import true_div
+from pytensor.tensor.math import true_divide
 from pytensor.tensor.rewriting.basic import register_specialize
 from pytensor.tensor.rewriting.math import local_mul_canonizer
 from pytensor.tensor.special import LogSoftmax, Softmax, SoftmaxGrad
@@ -53,7 +53,7 @@ def local_logsoftmax_grad(fgraph, node):
         isinstance(node.op, SoftmaxGrad)
         and len(node.inputs) == 2
         and node.inputs[0].owner is not None
-        and node.inputs[0].owner.op == true_div
+        and node.inputs[0].owner.op == true_divide
         and len(node.inputs[0].owner.inputs) >= 2
         and node.inputs[0].owner.inputs[1].owner is not None
         and isinstance(node.inputs[0].owner.inputs[1].owner.op, Softmax)
@@ -61,7 +61,7 @@ def local_logsoftmax_grad(fgraph, node):
         and not (
             # skip if it will be optimized by
             # local_advanced_indexing_crossentropy_onehot_grad
-            node.inputs[0].owner.op == true_div
+            node.inputs[0].owner.op == true_divide
             and node.inputs[0].owner.inputs[0].owner is not None
             and isinstance(
                 node.inputs[0].owner.inputs[0].owner.op, AdvancedIncSubtensor

tests/tensor/rewriting/test_basic.py
@@ -52,7 +52,7 @@ from pytensor.tensor.math import (
 from pytensor.tensor.math import pow as at_pow
 from pytensor.tensor.math import softplus, sqrt, sub
 from pytensor.tensor.math import sum as at_sum
-from pytensor.tensor.math import true_div
+from pytensor.tensor.math import true_divide
 from pytensor.tensor.rewriting.basic import (
     assert_op,
     local_alloc_sink_dimshuffle,
@@ -898,7 +898,7 @@ class TestLocalSwitchSink:
                 (dvector("x"), self.xv),
                 (dscalar("x"), self.xs),
             ]:
-                y = true_div(
+                y = true_divide(
                     at.switch(condition[0] > 0, 1.0 * x[0], 0.0 * x[0]),
                     at.switch(condition[0] > 0, 1.0 * x[0], log(c) * x[0]),
                 )
@@ -1083,7 +1083,7 @@ class TestLocalMergeSwitchSameCond:
             add,
             sub,
             mul,
-            true_div,
+            true_divide,
             int_div,
             floor_div,
             minimum,
@@ -1447,7 +1447,7 @@ def test_local_upcast_elemwise_constant_inputs():
     # This tests a corner case for which the rewrite should not be applied.
     with config.change_flags(floatX="float32"):
         v = lvector()
-        function([v], true_div(v, 2))
+        function([v], true_divide(v, 2))
 
 
 def test_assert_op_gradient():

tests/tensor/rewriting/test_special.py
@@ -9,7 +9,7 @@ from pytensor.configdefaults import config
 from pytensor.graph.fg import FunctionGraph
 from pytensor.graph.rewriting.basic import check_stack_trace
 from pytensor.graph.rewriting.db import RewriteDatabaseQuery
-from pytensor.tensor.math import add, exp, log, true_div
+from pytensor.tensor.math import add, exp, log, true_divide
 from pytensor.tensor.special import LogSoftmax, Softmax, SoftmaxGrad, softmax
 from pytensor.tensor.type import matrix
 from tests import unittest_tools as utt
@@ -78,7 +78,7 @@ class TestLogSoftmaxRewrites:
         softmax_grad_node = g.owner
         assert softmax_grad_node.op == SoftmaxGrad(axis=-1)
         true_div_node = softmax_grad_node.inputs[0].owner
-        assert true_div_node.op == true_div
+        assert true_div_node.op == true_divide
 
         # We replace thk elemwise true_div op by an elemwise add.
         new_g = SoftmaxGrad(axis=-1)(

tests/tensor/test_math.py
@@ -109,7 +109,7 @@ from pytensor.tensor.math import (
     sub,
 )
 from pytensor.tensor.math import sum as at_sum
-from pytensor.tensor.math import tan, tanh, tensordot, true_div, trunc, var
+from pytensor.tensor.math import tan, tanh, tensordot, true_divide, trunc, var
 from pytensor.tensor.type import (
     TensorType,
     complex_dtypes,
@@ -327,7 +327,7 @@ _grad_broadcast_div_mod_normal = dict(
 # fmt: on
 
 TestTrueDivBroadcast = makeBroadcastTester(
-    op=true_div,
+    op=true_divide,
     expected=_numpy_true_div,
     good=_good_broadcast_div_mod_normal_float_no_complex,
     grad=_grad_broadcast_div_mod_normal,
@@ -1864,8 +1864,8 @@ class TestDivimpl:
         assert np.allclose(function([i, f], f / i)(5, 11.0), (11.0 / 5.0))
         assert np.allclose(function([i, ii], i // ii)(5, 3), (5 // 3))
         assert np.allclose(function([i, ii], ii // i)(5, 3), (3 // 5))
-        assert np.allclose(function([i, ii], true_div(i, ii))(5, 3), (5.0 / 3.0))
-        assert np.allclose(function([i, ii], true_div(ii, i))(5, 3), (3.0 / 5.0))
+        assert np.allclose(function([i, ii], true_divide(i, ii))(5, 3), (5.0 / 3.0))
+        assert np.allclose(function([i, ii], true_divide(ii, i))(5, 3), (3.0 / 5.0))
         assert np.allclose(function([i, c], i / c)(5, complex(5, 3)), (5.0 / (5 + 3j)))
         assert np.allclose(function([i, c], c / i)(5, complex(5, 3)), ((5 + 3j) / 5.0))
 