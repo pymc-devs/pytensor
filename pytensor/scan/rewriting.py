9ab00a9c2fa0989578db40bd8f2715f68c2c68eb
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2023-03-08 12:59:02 +0100

    Test if branch of `save_mem_new_scan` is ever triggered by tests

--- pytensor/scan/rewriting.py
+++ pytensor/scan/rewriting.py
@@ -1573,6 +1573,7 @@ def save_mem_new_scan(fgraph, node):
         if global_nsteps is not None:
             for idx, val in enumerate(store_steps[op_info.n_mit_mot :]):
                 if val == 0:
+                    raise ValueError()
                     # val == 0 means that we want to keep all intermediate
                     # results for that state, including the initial values.
                     if idx < op_info.n_mit_sot + op_info.n_sit_sot:
