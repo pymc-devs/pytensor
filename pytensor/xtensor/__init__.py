024136e16281a6b5acbb5e8dde3fa4d159267dbc
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2023-08-02 14:59:22 +0200

    Implement basic labeled tensor functionality

---
+++ pytensor/xtensor/__init__.py
@@ -0,0 +1,12 @@
+import warnings
+
+import pytensor.xtensor.rewriting
+from pytensor.xtensor.type import (
+    XTensorType,
+    as_xtensor,
+    xtensor,
+    xtensor_constant,
+)
+
+
+warnings.warn("xtensor module is experimental and full of bugs")
