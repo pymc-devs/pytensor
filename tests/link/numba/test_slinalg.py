08de8faceae31487348031c04cb2e0bad0a925a0
jessegrabowski <jessegrabowski@gmail.com>
2023-08-28 02:09:14 +0200

    Add tolerance for float32 tests

--- tests/link/numba/test_slinalg.py
+++ tests/link/numba/test_slinalg.py
@@ -8,6 +8,10 @@ import pytensor.tensor as pt
 from pytensor import config
 
 
+ATOL = 0 if config.floatX.endswith('64') else 1e-6
+RTOL = 1e-7 if config.floatX.endswith('64') else 1e-6
+
+
 def test_solve_triangular():
     A = pt.matrix("A")
     b = pt.matrix("b")
@@ -22,7 +26,7 @@ def test_solve_triangular():
     b = np.random.normal(size=(5, 1)).astype(config.floatX)
 
     X_np = f(A_tri, b)
-    np.testing.assert_allclose(A_tri @ X_np, b)
+    np.testing.assert_allclose(A_tri @ X_np, b, atol=ATOL, rtol=RTOL)
 
 
 def test_scipy_solve_triangular_not_overloaded():
