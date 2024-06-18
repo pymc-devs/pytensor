74c64c615d5d52825c218278ea2da09a4f05ea67
Chris Fonnesbeck <fonnesbeck@gmail.com>
2024-06-18 09:31:55 -0400

    Update pytensor/tensor/math.py
    Co-authored-by: Ricardo Vieira <28983449+ricardoV94@users.noreply.github.com>

--- pytensor/tensor/math.py
+++ pytensor/tensor/math.py
@@ -698,7 +698,13 @@ def ge(a, b):
 
 
 def eq(a, b):
-    """Elementwise equality comparison, `a == b`."""
+    """Elementwise equality comparison, `a == b`.
+    
+    Notes
+    -----
+    Due to Python rules, it is not possible to correctly overload the equality symbol `==` for hashable objects,  
+    so `eq` must always be used to compute the Elemwise equality of TensorVariables (which are hashable).
+    """
 
 
 def neq(a, b):
