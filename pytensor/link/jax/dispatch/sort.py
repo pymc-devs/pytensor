3dd1f80fe341ac525005f1ff8f0f0822dd25e2f1
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2024-06-24 13:34:00 +0200

    Remove useless order kwarg from Sort and add numpy 2.0 stable kwarg

--- pytensor/link/jax/dispatch/sort.py
+++ pytensor/link/jax/dispatch/sort.py
@@ -6,7 +6,9 @@ from pytensor.tensor.sort import SortOp
 
 @jax_funcify.register(SortOp)
 def jax_funcify_Sort(op, **kwargs):
+    stable = op.kind == "stable"
+
     def sort(arr, axis):
-        return jnp.sort(arr, axis=axis)
+        return jnp.sort(arr, axis=axis, stable=stable)
 
     return sort
