9cf815e44f71656e08abd7ad1225a35c2ac68eb7
Virgile Andreani <virgile@pymc-devs.org>
2024-07-03 05:49:56 -0400

    Make non-strict zip strict in test_abstract_conv

--- tests/tensor/conv/test_abstract_conv.py
+++ tests/tensor/conv/test_abstract_conv.py
@@ -1745,7 +1745,7 @@ class TestConv2dGrads:
         self.random_stream = np.random.default_rng(utt.fetch_seed())
 
         self.inputs_shapes = [(8, 1, 12, 12), (1, 1, 5, 5), (1, 1, 5, 6), (1, 1, 6, 6)]
-        self.filters_shapes = [(5, 1, 2, 2), (1, 1, 3, 3)]
+        self.filters_shapes = [(5, 1, 2, 2), (1, 1, 3, 3)] * 2
 
         self.subsamples = [(1, 1), (2, 2)]
         self.border_modes = ["valid", "full"]
@@ -1764,7 +1764,7 @@ class TestConv2dGrads:
         # results are the same.
 
         for in_shape, fltr_shape in zip(
-            self.inputs_shapes, self.filters_shapes, strict=False
+            self.inputs_shapes, self.filters_shapes, strict=True
         ):
             for bm in self.border_modes:
                 for ss in self.subsamples:
@@ -1828,7 +1828,7 @@ class TestConv2dGrads:
         # results are the same.
 
         for in_shape, fltr_shape in zip(
-            self.inputs_shapes, self.filters_shapes, strict=False
+            self.inputs_shapes, self.filters_shapes, strict=True
         ):
             for bm in self.border_modes:
                 for ss in self.subsamples:
