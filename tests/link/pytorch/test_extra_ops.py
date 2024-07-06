9a14dba31af657ea40a69192fd7cc41aada1ddda
Diego Sandoval <dsandovalv@gmail.com>
2024-07-06 07:17:59 +0000

    Implemented Repeat and Unique Ops in PyTorch

--- tests/link/pytorch/test_extra_ops.py
+++ tests/link/pytorch/test_extra_ops.py
@@ -41,3 +41,49 @@ def test_pytorch_CumOp(axis, dtype):
         out = pt.cumprod(a, axis=axis)
         fgraph = FunctionGraph([a], [out])
         compare_pytorch_and_py(fgraph, [test_value])
+
+
+def test_pytorch_Repeat():
+    a = pt.matrix("a", dtype="float64")
+
+    test_value = np.arange(6, dtype="float64").reshape((3, 2))
+
+    # Test along axis 0
+    out = pt.repeat(a, (1, 2, 3), axis=0)
+    fgraph = FunctionGraph([a], [out])
+    compare_pytorch_and_py(fgraph, [test_value])
+
+    # Test along axis 1
+    out = pt.repeat(a, (3, 3), axis=1)
+    fgraph = FunctionGraph([a], [out])
+    compare_pytorch_and_py(fgraph, [test_value])
+
+
+def test_pytorch_Unique():
+    a = pt.matrix("a", dtype="float64")
+
+    test_value = np.array(
+        [[1.0, 1.0, 2.0], [1.0, 1.0, 2.0], [3.0, 3.0, 0.0]], dtype="float64"
+    )
+
+    # Test along axis 0
+    out = pt.unique(a, axis=0)
+    fgraph = FunctionGraph([a], [out])
+    compare_pytorch_and_py(fgraph, [test_value])
+
+    # Test along axis 1
+    out = pt.unique(a, axis=1)
+    fgraph = FunctionGraph([a], [out])
+    compare_pytorch_and_py(fgraph, [test_value])
+
+    # Test with params
+    out = pt.unique(a, return_inverse=True, return_counts=True, axis=0)
+    fgraph = FunctionGraph([a], [out[0]])
+    compare_pytorch_and_py(fgraph, [test_value])
+
+    # Test with return_index=True
+    out = pt.unique(a, return_index=True, axis=0)
+    fgraph = FunctionGraph([a], [out[0]])
+
+    with pytest.raises(NotImplementedError):
+        compare_pytorch_and_py(fgraph, [test_value])
