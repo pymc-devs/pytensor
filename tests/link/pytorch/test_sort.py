4a33301612fee74777d9565292a6d3bf68951bd2
Diego Sandoval <dsandovalv@gmail.com>
2024-07-08 00:08:55 +0200

    Marked test for [Arg]Sort Op in PyTorch as xfail

--- tests/link/pytorch/test_sort.py
+++ tests/link/pytorch/test_sort.py
@@ -7,6 +7,7 @@ from pytensor.tensor.sort import argsort, sort
 from tests.link.pytorch.test_basic import compare_pytorch_and_py
 
 
+@pytest.mark.xfail(reason="Reshape not implemented")
 @pytest.mark.parametrize("axis", [0, 1, None])
 @pytest.mark.parametrize("func", (sort, argsort))
 def test_sort(func, axis):
@@ -14,10 +15,4 @@ def test_sort(func, axis):
     out = func(x, axis=axis)
     fgraph = FunctionGraph([x], [out])
     arr = np.array([[1.0, 4.0], [5.0, 2.0]])
-
-    # TODO: remove condition once Reshape is implemented
-    if axis is None:
-        with pytest.raises(NotImplementedError):
-            compare_pytorch_and_py(fgraph, [arr])
-    else:
-        compare_pytorch_and_py(fgraph, [arr])
+    compare_pytorch_and_py(fgraph, [arr])
