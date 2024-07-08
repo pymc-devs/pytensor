03bb3a80b54f3f9c6d832bb2806cec1fbab13648
HangenYuu <pnhung1703@gmail.com>
2024-07-08 17:57:26 +0800

    Reverted logic to correct scope for math.dot

--- pytensor/link/pytorch/dispatch/nlinalg.py
+++ pytensor/link/pytorch/dispatch/nlinalg.py
@@ -7,11 +7,6 @@ from pytensor.tensor.math import Dot
 @pytorch_funcify.register(Dot)
 def pytorch_funcify_Dot(op, **kwargs):
     def dot(x, y):
-        # Case 1: Vector Product/Matrix Multiplication/1-D Broadcastable Vector
-        if x.shape == 1 or y.shape == 1 or (x.shape < 3 and y.shape < 3):
-            return torch.matmul(x, y)
-        else:
-            # Case 2: Stackable batch dimension
-            return torch.tensordot(x, y, dims=([-1], [-2]))
+        return torch.matmul(x, y)
 
     return dot
--- tests/link/pytorch/test_nlinalg.py
+++ tests/link/pytorch/test_nlinalg.py
@@ -3,15 +3,11 @@ import numpy as np
 from pytensor.configdefaults import config
 from pytensor.graph.fg import FunctionGraph
 from pytensor.graph.op import get_test_value
-from pytensor.tensor.type import matrix, scalar, tensor3, vector
+from pytensor.tensor.type import matrix, scalar, vector
 from tests.link.pytorch.test_basic import compare_pytorch_and_py
 
 
 def test_pytorch_dot():
-    a = tensor3("a")
-    a.tag.test_value = np.zeros((3, 2, 4)).astype(config.floatX)
-    b = tensor3("b")
-    b.tag.test_value = np.zeros((3, 4, 1)).astype(config.floatX)
     y = vector("y")
     y.tag.test_value = np.r_[1.0, 2.0].astype(config.floatX)
     x = vector("x")
@@ -22,11 +18,6 @@ def test_pytorch_dot():
     alpha.tag.test_value = np.array(3.0, dtype=config.floatX)
     beta = scalar("beta")
     beta.tag.test_value = np.array(5.0, dtype=config.floatX)
-
-    # 3D * 3D
-    out = a.dot(b * alpha) + beta * b
-    fgraph = FunctionGraph([a, b, alpha, beta], [out])
-    compare_pytorch_and_py(fgraph, [get_test_value(i) for i in fgraph.inputs])
 
     # 2D * 2D
     out = A.dot(A * alpha) + beta * A
