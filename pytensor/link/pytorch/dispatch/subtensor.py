6e8fe6479f5d72e085db104933c895da6dcb1eab
HarshvirSandhu <harshvir2173@gmail.com>
2024-07-10 00:15:27 +0530

    Implement Subtensor and AdvancedSubtensor Ops

---
+++ pytensor/link/pytorch/dispatch/subtensor.py
@@ -0,0 +1,38 @@
+from pytensor.link.pytorch.dispatch.basic import pytorch_funcify
+from pytensor.tensor.subtensor import (
+    AdvancedSubtensor,
+    AdvancedSubtensor1,
+    Subtensor,
+    indices_from_subtensor,
+)
+
+
+@pytorch_funcify.register(Subtensor)
+@pytorch_funcify.register(AdvancedSubtensor)
+@pytorch_funcify.register(AdvancedSubtensor1)
+def pytorch_funcify_Subtensor(op, node, **kwargs):
+    idx_list = getattr(op, "idx_list", None)
+
+    def subtensor(x, *ilists):
+        indices = indices_from_subtensor(ilists, idx_list)
+        new_indices = []
+
+        for i in indices:
+            new_start, new_step, new_stop = None, None, None
+            if isinstance(i, slice):
+                if i.start:
+                    new_start = i.start.item()
+                if i.step:
+                    new_step = i.step.item()
+                if i.stop:
+                    new_stop = i.stop.item()
+                new_indices.append(slice(new_start, new_stop, new_step))
+            else:
+                new_indices.append(i.tolist())
+
+        if len(indices) == 1:
+            indices = indices[0]
+
+        return x[*new_indices]
+
+    return subtensor
---
+++ tests/link/pytorch/test_subtensor.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+from pytensor.graph.fg import FunctionGraph
+from pytensor.tensor import subtensor as pt_subtensor
+from pytensor.tensor import tensor
+from tests.link.pytorch.test_basic import compare_pytorch_and_py
+
+
+def test_pytorch_Subtensor():
+    shape = (3, 4, 5)
+    x_pt = tensor("x", shape=shape, dtype="int")
+    x_np = np.arange(np.prod(shape)).reshape(shape)
+
+    # Basic indices
+    out_pt = x_pt[1, 2, 0]
+    assert isinstance(out_pt.owner.op, pt_subtensor.Subtensor)
+    out_fg = FunctionGraph([x_pt], [out_pt])
+    compare_pytorch_and_py(out_fg, [x_np])
+
+    out_pt = x_pt[1:, 1, :]
+    assert isinstance(out_pt.owner.op, pt_subtensor.Subtensor)
+    out_fg = FunctionGraph([x_pt], [out_pt])
+    compare_pytorch_and_py(out_fg, [x_np])
+
+    out_pt = x_pt[:2, 1, :]
+    assert isinstance(out_pt.owner.op, pt_subtensor.Subtensor)
+    out_fg = FunctionGraph([x_pt], [out_pt])
+    compare_pytorch_and_py(out_fg, [x_np])
+
+    out_pt = x_pt[1:2, 1, :]
+    assert isinstance(out_pt.owner.op, pt_subtensor.Subtensor)
+    out_fg = FunctionGraph([x_pt], [out_pt])
+    compare_pytorch_and_py(out_fg, [x_np])
+
+    # Advanced indexing
+    out_pt = pt_subtensor.advanced_subtensor1(x_pt, [1, 2])
+    assert isinstance(out_pt.owner.op, pt_subtensor.AdvancedSubtensor1)
+    out_fg = FunctionGraph([x_pt], [out_pt])
+    compare_pytorch_and_py(out_fg, [x_np])
+
+    out_pt = x_pt[[1, 2], [2, 3]]
+    assert isinstance(out_pt.owner.op, pt_subtensor.AdvancedSubtensor)
+    out_fg = FunctionGraph([x_pt], [out_pt])
+    compare_pytorch_and_py(out_fg, [x_np])
+
+    # Advanced and basic indexing
+    out_pt = x_pt[[1, 2], :]
+    assert isinstance(out_pt.owner.op, pt_subtensor.AdvancedSubtensor)
+    out_fg = FunctionGraph([x_pt], [out_pt])
+    compare_pytorch_and_py(out_fg, [x_np])
+
+    # out_pt = x_pt[[1, 2], :, [3, 4]]
+    # assert isinstance(out_pt.owner.op, pt_subtensor.AdvancedSubtensor)
+    # out_fg = FunctionGraph([x_pt], [out_pt])
+    # compare_pytorch_and_py(out_fg, [x_np])
+
+    # Flipping
+    # out_pt = x_pt[::-1]
+    # out_fg = FunctionGraph([x_pt], [out_pt])
+    # compare_pytorch_and_py(out_fg, [x_np])
+
+    # Boolean indexing should work if indexes are constant
+    out_pt = x_pt[np.random.binomial(1, 0.5, size=(3, 4, 5)).astype(bool)]
+    out_fg = FunctionGraph([x_pt], [out_pt])
+    compare_pytorch_and_py(out_fg, [x_np])
