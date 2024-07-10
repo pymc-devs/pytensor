7feae8a848fea388208e7519a297aac0daa286e4
Tanish Taneja <tanish.taneja@research.iiit.ac.in>
2024-07-10 17:51:57 +0530

    updated check

--- pytensor/tensor/rewriting/linalg.py
+++ pytensor/tensor/rewriting/linalg.py
@@ -581,12 +581,12 @@ def _find_solve_with_eye(node):
         return False
     # Check whether second input to solve is Eye
     solve_inputs = node.inputs
-    potential_eye_input = solve_inputs[1].owner
+    eye_input = solve_inputs[1].owner
 
-    if not (potential_eye_input and isinstance(potential_eye_input.op, Eye)):
+    if not (eye_input and isinstance(eye_input.op, Eye)):
         return False
 
-    if getattr(potential_eye_input.inputs[-1], "data", -1).item() != 0:
+    if eye_input.inputs[-1].data.item() != 0:
         return False
     return True
 
