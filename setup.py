6d1546cdfd67052998b96f22bf0609dc5a5c03a8
Thomas Wiecki <thomas.wiecki@gmail.com>
2024-07-10 18:25:40 +0300

    Add building of pyodide universal wheels

--- setup.py
+++ setup.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python
 import numpy
-import versioneer
 from setuptools import Extension, setup
 from setuptools.dist import Distribution
-
+import os
+import versioneer
 
 dist = Distribution()
 dist.parse_config_files()
 
+NAME = dist.get_name()  # type: ignore
 
-NAME: str = dist.get_name()  # type: ignore
+# Check if building for Pyodide
+is_pyodide = os.getenv('PYODIDE', '0') == '1'
 
+# Define the ext_modules conditionally
+ext_modules = []
+if not is_pyodide:
+    ext_modules = [
+        Extension(
+            name="pytensor.scan.scan_perform",
+            sources=["pytensor/scan/scan_perform.pyx"],
+            include_dirs=[numpy.get_include()],
+        ),
+    ]
 
 if __name__ == "__main__":
     setup(
         name=NAME,
         version=versioneer.get_version(),
         cmdclass=versioneer.get_cmdclass(),
-        ext_modules=[
-            Extension(
-                name="pytensor.scan.scan_perform",
-                sources=["pytensor/scan/scan_perform.pyx"],
-                include_dirs=[numpy.get_include()],
-            ),
-        ],
+        ext_modules=ext_modules,
     )
