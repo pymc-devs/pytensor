3f8a3776fc782546123cdd281188bf8e66488877
Jesse Grabowski <jessegrabowski@gmail.com>
2024-07-14 01:44:10 +0800

    Appease mypy

--- pytensor/link/jax/dispatch/pad.py
+++ pytensor/link/jax/dispatch/pad.py
@@ -13,33 +13,41 @@ def jax_funcify_pad(op, **kwargs):
 
     if pad_mode == "constant":
 
-        def pad(x, pad_width, constant_values):
+        def constant_pad(x, pad_width, constant_values):
             return jnp.pad(x, pad_width, mode=pad_mode, constant_values=constant_values)
+
+        return constant_pad
 
     elif pad_mode == "linear_ramp":
 
-        def pad(x, pad_width, end_values):
+        def lr_pad(x, pad_width, end_values):
             # JAX does not allow a dynamic input if end_values is non-scalar
             if not isinstance(end_values, int | float):
                 end_values = tuple(np.array(end_values))
             return jnp.pad(x, pad_width, mode=pad_mode, end_values=end_values)
 
+        return lr_pad
+
     elif pad_mode in ["maximum", "minimum", "mean"] and has_stat_length:
 
-        def pad(x, pad_width, stat_length):
+        def stat_pad(x, pad_width, stat_length):
             # JAX does not allow a dynamic input here, need to cast to tuple
             return jnp.pad(
                 x, pad_width, mode=pad_mode, stat_length=tuple(np.array(stat_length))
             )
 
+        return stat_pad
+
     elif pad_mode in ["reflect", "symmetric"]:
 
-        def pad(x, pad_width):
+        def loop_pad(x, pad_width):
             return jnp.pad(x, pad_width, mode=pad_mode, reflect_type=reflect_type)
+
+        return loop_pad
 
     else:
 
         def pad(x, pad_width):
             return jnp.pad(x, pad_width, mode=pad_mode)
 
-    return pad
+        return pad
