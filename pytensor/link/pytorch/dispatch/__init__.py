2cf0ed2580cb2b1264ba562f3cd58494ef352925
HangenYuu <pnhung1703@gmail.com>
2024-07-15 10:27:35 +0800

    Reverted folder structure and added BatchedDot

--- pytensor/link/pytorch/dispatch/__init__.py
+++ pytensor/link/pytorch/dispatch/__init__.py
@@ -2,6 +2,9 @@
 from pytensor.link.pytorch.dispatch.basic import pytorch_funcify, pytorch_typify
 
 # # Load dispatch specializations
+import pytensor.link.pytorch.dispatch.blas
 import pytensor.link.pytorch.dispatch.scalar
 import pytensor.link.pytorch.dispatch.elemwise
+import pytensor.link.pytorch.dispatch.math
+
 # isort: on
