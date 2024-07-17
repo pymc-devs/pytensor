4deea70cc21a174a5eb93122fdb112c6867f1963
HangenYuu <pnhung1703@gmail.com>
2024-07-17 16:01:06 +0800

    Removed get_test_value

--- tests/link/pytorch/test_blas.py
+++ tests/link/pytorch/test_blas.py
@@ -1,11 +1,8 @@
 import numpy as np
 import pytest
 
-from pytensor.compile.mode import Mode
 from pytensor.configdefaults import config
 from pytensor.graph.fg import FunctionGraph
-from pytensor.graph.op import get_test_value
-from pytensor.link.pytorch.linker import PytorchLinker
 from pytensor.tensor import blas as pt_blas
 from pytensor.tensor.type import tensor3
 from tests.link.pytorch.test_basic import compare_pytorch_and_py
@@ -14,22 +11,14 @@ from tests.link.pytorch.test_basic import compare_pytorch_and_py
 def test_pytorch_BatchedDot():
     # tensor3 . tensor3
     a = tensor3("a")
-    a.tag.test_value = (
-        np.linspace(-1, 1, 10 * 5 * 3).astype(config.floatX).reshape((10, 5, 3))
-    )
+    A = np.linspace(-1, 1, 10 * 5 * 3).astype(config.floatX).reshape((10, 5, 3))
     b = tensor3("b")
-    b.tag.test_value = (
-        np.linspace(1, -1, 10 * 3 * 2).astype(config.floatX).reshape((10, 3, 2))
-    )
+    B = np.linspace(1, -1, 10 * 3 * 2).astype(config.floatX).reshape((10, 3, 2))
     out = pt_blas.BatchedDot()(a, b)
     fgraph = FunctionGraph([a, b], [out])
-    pytensor_pytorch_fn, _ = compare_pytorch_and_py(
-        fgraph, [get_test_value(i) for i in fgraph.inputs]
-    )
+    pytensor_pytorch_fn, _ = compare_pytorch_and_py(fgraph, [A, B])
 
     # A dimension mismatch should raise a TypeError for compatibility
-    inputs = [get_test_value(a)[:-1], get_test_value(b)]
-    pytorch_mode_no_rewrites = Mode(PytorchLinker(), None)
-    pytensor_pytorch_fn.mode = pytorch_mode_no_rewrites
+    inputs = [A[:-1], B]
     with pytest.raises(TypeError):
         pytensor_pytorch_fn(*inputs)
--- tests/link/pytorch/test_math.py
+++ tests/link/pytorch/test_math.py
@@ -2,29 +2,28 @@ import numpy as np
 
 from pytensor.configdefaults import config
 from pytensor.graph.fg import FunctionGraph
-from pytensor.graph.op import get_test_value
 from pytensor.tensor.type import matrix, scalar, vector
 from tests.link.pytorch.test_basic import compare_pytorch_and_py
 
 
 def test_pytorch_dot():
     y = vector("y")
-    y.tag.test_value = np.r_[1.0, 2.0].astype(config.floatX)
+    y_value = np.r_[1.0, 2.0].astype(config.floatX)
     x = vector("x")
-    x.tag.test_value = np.r_[3.0, 4.0].astype(config.floatX)
+    x_value = np.r_[3.0, 4.0].astype(config.floatX)
     A = matrix("A")
-    A.tag.test_value = np.array([[6, 3], [3, 0]], dtype=config.floatX)
+    A_value = np.array([[6, 3], [3, 0]], dtype=config.floatX)
     alpha = scalar("alpha")
-    alpha.tag.test_value = np.array(3.0, dtype=config.floatX)
+    alpha_value = np.array(3.0, dtype=config.floatX)
     beta = scalar("beta")
-    beta.tag.test_value = np.array(5.0, dtype=config.floatX)
+    beta_value = np.array(5.0, dtype=config.floatX)
 
     # 2D * 2D
     out = A.dot(A * alpha) + beta * A
     fgraph = FunctionGraph([A, alpha, beta], [out])
-    compare_pytorch_and_py(fgraph, [get_test_value(i) for i in fgraph.inputs])
+    compare_pytorch_and_py(fgraph, [A_value, alpha_value, beta_value])
 
     # 1D * 2D and 1D * 1D
     out = y.dot(alpha * A).dot(x) + beta * y
     fgraph = FunctionGraph([y, x, A, alpha, beta], [out])
-    compare_pytorch_and_py(fgraph, [get_test_value(i) for i in fgraph.inputs])
+    compare_pytorch_and_py(fgraph, [y_value, x_value, A_value, alpha_value, beta_value])
