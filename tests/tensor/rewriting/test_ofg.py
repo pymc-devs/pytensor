6487e61dcb43cf723b516f704f067cc53290fe14
Jesse Grabowski <jessegrabowski@gmail.com>
2024-07-17 12:53:56 +0800

    Skip inline rewrite tests when `mode == FAST_COMPILE`

--- tests/tensor/rewriting/test_ofg.py
+++ tests/tensor/rewriting/test_ofg.py
@@ -1,8 +1,15 @@
+import pytest
+
 import pytensor
 import pytensor.tensor as pt
+from pytensor import config
 from pytensor.compile.builders import OpFromGraph
 
 
+@pytest.mark.skipif(
+    config.mode == "FAST_COMPILE",
+    reason="Rewrite is not applied in FAST_COMPILE mode",
+)
 def test_alloc_diag_inlined():
     x = pt.tensor("x", shape=(None,))
 
