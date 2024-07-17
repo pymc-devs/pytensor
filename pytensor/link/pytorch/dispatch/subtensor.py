7f9f6dc89467875261ea8109233f176f7d256e6f
HarshvirSandhu <harshvir2173@gmail.com>
2024-07-18 00:07:27 +0530

    Implement IncSubtensor and AdvancedIncSubtensor

--- pytensor/link/pytorch/dispatch/subtensor.py
+++ pytensor/link/pytorch/dispatch/subtensor.py
@@ -1,7 +1,9 @@
 from pytensor.link.pytorch.dispatch.basic import pytorch_funcify
 from pytensor.tensor.subtensor import (
+    AdvancedIncSubtensor1,
     AdvancedSubtensor,
     AdvancedSubtensor1,
+    IncSubtensor,
     Subtensor,
     indices_from_subtensor,
 )
@@ -40,3 +42,28 @@ def pytorch_funcify_Subtensor(op, node, **kwargs):
         return x[tuple(new_indices)]
 
     return subtensor
+
+
+@pytorch_funcify.register(IncSubtensor)
+@pytorch_funcify.register(AdvancedIncSubtensor1)
+def pytorch_funcify_IncSubtensor(op, node, **kwargs):
+    idx_list = getattr(op, "idx_list", None)
+
+    if getattr(op, "set_instead_of_inc", False):
+
+        def torch_fn(x, indices, y):
+            return x.at[indices].set(y)
+
+    else:
+
+        def torch_fn(x, indices, y):
+            return x.at[indices].add(y)
+
+    def incsubtensor(x, y, *ilist, torch_fn=torch_fn, idx_list=idx_list):
+        indices = indices_from_subtensor(ilist, idx_list)
+        if len(indices) == 1:
+            indices = indices[0]
+
+        return torch_fn(x, indices, y)
+
+    return incsubtensor
