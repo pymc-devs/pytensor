cab9db8bf54233515e079e878f806595a7f52d2f
HangenYuu <pnhung1703@gmail.com>
2024-07-18 09:26:58 +0800

    Changed variable names

--- tests/link/pytorch/test_blas.py
+++ tests/link/pytorch/test_blas.py
@@ -11,14 +11,14 @@ from tests.link.pytorch.test_basic import compare_pytorch_and_py
 def test_pytorch_BatchedDot():
     # tensor3 . tensor3
     a = tensor3("a")
-    A = np.linspace(-1, 1, 10 * 5 * 3).astype(config.floatX).reshape((10, 5, 3))
+    a_test = np.linspace(-1, 1, 10 * 5 * 3).astype(config.floatX).reshape((10, 5, 3))
     b = tensor3("b")
-    B = np.linspace(1, -1, 10 * 3 * 2).astype(config.floatX).reshape((10, 3, 2))
+    b_test = np.linspace(1, -1, 10 * 3 * 2).astype(config.floatX).reshape((10, 3, 2))
     out = pt_blas.BatchedDot()(a, b)
     fgraph = FunctionGraph([a, b], [out])
-    pytensor_pytorch_fn, _ = compare_pytorch_and_py(fgraph, [A, B])
+    pytensor_pytorch_fn, _ = compare_pytorch_and_py(fgraph, [a_test, b_test])
 
     # A dimension mismatch should raise a TypeError for compatibility
-    inputs = [A[:-1], B]
+    inputs = [a_test[:-1], b_test]
     with pytest.raises(TypeError):
         pytensor_pytorch_fn(*inputs)
--- tests/link/pytorch/test_math.py
+++ tests/link/pytorch/test_math.py
@@ -8,22 +8,22 @@ from tests.link.pytorch.test_basic import compare_pytorch_and_py
 
 def test_pytorch_dot():
     y = vector("y")
-    y_value = np.r_[1.0, 2.0].astype(config.floatX)
+    y_test = np.r_[1.0, 2.0].astype(config.floatX)
     x = vector("x")
-    x_value = np.r_[3.0, 4.0].astype(config.floatX)
+    x_test = np.r_[3.0, 4.0].astype(config.floatX)
     A = matrix("A")
-    A_value = np.array([[6, 3], [3, 0]], dtype=config.floatX)
+    A_test = np.array([[6, 3], [3, 0]], dtype=config.floatX)
     alpha = scalar("alpha")
-    alpha_value = np.array(3.0, dtype=config.floatX)
+    alpha_test = np.array(3.0, dtype=config.floatX)
     beta = scalar("beta")
-    beta_value = np.array(5.0, dtype=config.floatX)
+    beta_test = np.array(5.0, dtype=config.floatX)
 
     # 2D * 2D
     out = A.dot(A * alpha) + beta * A
     fgraph = FunctionGraph([A, alpha, beta], [out])
-    compare_pytorch_and_py(fgraph, [A_value, alpha_value, beta_value])
+    compare_pytorch_and_py(fgraph, [A_test, alpha_test, beta_test])
 
     # 1D * 2D and 1D * 1D
     out = y.dot(alpha * A).dot(x) + beta * y
     fgraph = FunctionGraph([y, x, A, alpha, beta], [out])
-    compare_pytorch_and_py(fgraph, [y_value, x_value, A_value, alpha_value, beta_value])
+    compare_pytorch_and_py(fgraph, [y_test, x_test, A_test, alpha_test, beta_test])
