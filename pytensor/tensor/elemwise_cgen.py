061ed3acdfb5eb14b0e04d9c504df3cb64194b4c
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2024-08-04 19:17:05 +0200

    CAReduce loop reordering C-impl

--- pytensor/tensor/elemwise_cgen.py
+++ pytensor/tensor/elemwise_cgen.py
@@ -1,9 +1,10 @@
+from collections.abc import Sequence
 from textwrap import dedent, indent
 
 from pytensor.configdefaults import config
 
 
-def make_declare(loop_orders, dtypes, sub):
+def make_declare(loop_orders, dtypes, sub, compute_stride_jump=True):
     """
     Produce code to declare all necessary variables.
 
@@ -20,13 +21,11 @@ def make_declare(loop_orders, dtypes, sub):
                 # the number of elements in that dimension,
                 # the stride in that dimension,
                 # and the jump from an iteration to the next
-                decl += f"""
-                npy_intp {var}_n{value};
-                ssize_t {var}_stride{value};
-                int {var}_jump{value}_{j};
-                """
+                decl += f"npy_intp {var}_n{value};\nssize_t {var}_stride{value};\n"
+                if compute_stride_jump:
+                    decl += f"int {var}_jump{value}_{j};\n"
 
-            else:
+            elif compute_stride_jump:
                 # if the dimension is broadcasted, we only need
                 # the jump (arbitrary length and stride = 0)
                 decl += f"int {var}_jump{value}_{j};\n"
@@ -34,7 +33,7 @@ def make_declare(loop_orders, dtypes, sub):
     return decl
 
 
-def make_checks(loop_orders, dtypes, sub):
+def make_checks(loop_orders, dtypes, sub, compute_stride_jump=True):
     init = ""
     for i, (loop_order, dtype) in enumerate(zip(loop_orders, dtypes)):
         var = sub[f"lv{i}"]
@@ -67,13 +66,13 @@ def make_checks(loop_orders, dtypes, sub):
                 # Initialize the variables associated to the jth loop
                 # jump = stride - adjust
                 jump = f"({var}_stride{index}) - ({adjust})"
-                init += f"""
-                {var}_n{index} = PyArray_DIMS({var})[{index}];
-                {var}_stride{index} = PyArray_STRIDES({var})[{index}] / sizeof({dtype});
-                {var}_jump{index}_{j} = {jump};
-                """
+                init += f"{var}_n{index} = PyArray_DIMS({var})[{index}];\n"
+                init += f"{var}_stride{index} = PyArray_STRIDES({var})[{index}] / sizeof({dtype});\n"
+                if compute_stride_jump:
+                    init += f"{var}_jump{index}_{j} = {jump};\n"
                 adjust = f"{var}_n{index}*{var}_stride{index}"
-            else:
+
+            elif compute_stride_jump:
                 jump = f"-({adjust})"
                 init += f"{var}_jump{index}_{j} = {jump};\n"
                 adjust = "0"
@@ -460,72 +459,298 @@ def make_reordered_loop(
 ################
 
 
-def make_loop_careduce(loop_orders, dtypes, loop_tasks, sub):
+def make_complete_loop_careduce(
+    inp_var: str,
+    acc_var: str,
+    inp_dtype: str,
+    acc_dtype: str,
+    initial_value: str,
+    inner_task: str,
+    fail_code,
+) -> str:
+    """Generate C code for a complete reduction loop.
+
+    The generated code for a float64 input variable `inp` and accumulation variable `acc` looks like:
+
+    .. code-block:: C
+        {
+            NpyIter* iter;
+            NpyIter_IterNextFunc *iternext;
+            char** data_ptr;
+            npy_intp* stride_ptr,* innersize_ptr;
+
+            // Special case for empty inputs
+            if (PyArray_SIZE(inp) == 0) {
+                npy_float64 acc_i = *(npy_float64*)(PyArray_DATA(acc));
+                acc_i = 0;
+            }else{
+                iter = NpyIter_New(inp,
+                                   NPY_ITER_READONLY| NPY_ITER_EXTERNAL_LOOP| NPY_ITER_REFS_OK,
+                                   NPY_KEEPORDER,
+                                   NPY_NO_CASTING,
+                                   NULL);
+                iternext = NpyIter_GetIterNext(iter, NULL);
+                if (iternext == NULL) {
+                    NpyIter_Deallocate(iter);
+                    { fail }
+                }
+                data_ptr = NpyIter_GetDataPtrArray(iter);
+                stride_ptr = NpyIter_GetInnerStrideArray(iter);
+                innersize_ptr = NpyIter_GetInnerLoopSizePtr(iter);
+
+                npy_float64 acc_i;
+                acc_i = 0;
+                do {
+                    char* data = *data_ptr;
+                    npy_intp stride = *stride_ptr;
+                    npy_intp count = *innersize_ptr;
+
+                    while(count--) {
+                        npy_float64 inp_i = *((npy_float64*)data);
+                        acc_i = acc_i + inp_i;
+                        data += stride;
+                    }
+
+                } while(iternext(iter));
+                NpyIter_Deallocate(iter);
+
+                *(npy_float64*)(PyArray_DATA(acc)) = acc_i;
+            }
+        }
     """
-    Make a nested loop over several arrays and associate specific code
-    to each level of nesting.
+    return dedent(
+        f"""
+        {{
+            NpyIter* iter;
+            NpyIter_IterNextFunc *iternext;
+            char** data_ptr;
+            npy_intp* stride_ptr,* innersize_ptr;
 
-    Parameters
-    ----------
-    loop_orders : list of N tuples of length M
-        Each value of each tuple can be either the index of a dimension to
-        loop over or the letter 'x' which means there is no looping to be done
-        over that variable at that point (in other words we broadcast
-        over that dimension). If an entry is an integer, it will become
-        an alias of the entry of that rank.
-    loop_tasks : list of M+1 pieces of code
-        The ith loop_task is a pair of strings, the first
-        string is code to be executed before the ith loop starts, the second
-        one contains code to be executed just before going to the next element
-        of the ith dimension.
-        The last element if loop_tasks is a single string, containing code
-        to be executed at the very end.
-    sub: dictionary
-        Maps 'lv#' to a suitable variable name.
-        The 'lvi' variable corresponds to the ith element of loop_orders.
+            // Special case for empty inputs
+            if (PyArray_SIZE({inp_var}) == 0) {{
+                {acc_dtype} &{acc_var}_i = *({acc_dtype}*)(PyArray_DATA({acc_var}));
+                {initial_value}
+            }}else{{
+                iter = NpyIter_New({inp_var},
+                                   NPY_ITER_READONLY| NPY_ITER_EXTERNAL_LOOP| NPY_ITER_REFS_OK,
+                                   NPY_KEEPORDER,
+                                   NPY_NO_CASTING,
+                                   NULL);
 
-    """
+                iternext = NpyIter_GetIterNext(iter, NULL);
+                if (iternext == NULL) {{
+                    NpyIter_Deallocate(iter);
+                    {fail_code}
+                }}
 
-    def loop_over(preloop, code, indices, i):
-        iterv = f"ITER_{int(i)}"
-        update = ""
-        suitable_n = "1"
-        for j, index in enumerate(indices):
-            var = sub[f"lv{int(j)}"]
-            update += f"{var}_iter += {var}_jump{index}_{i};\n"
-            if index != "x":
-                suitable_n = f"{var}_n{index}"
-        return f"""
-        {preloop}
-        for (int {iterv} = {suitable_n}; {iterv}; {iterv}--) {{
-            {code}
-            {update}
+                data_ptr = NpyIter_GetDataPtrArray(iter);
+                stride_ptr = NpyIter_GetInnerStrideArray(iter);
+                innersize_ptr = NpyIter_GetInnerLoopSizePtr(iter);
+
+                {acc_dtype} {acc_var}_i;
+                {initial_value}
+
+                do {{
+                    char* data = *data_ptr;
+                    npy_intp stride = *stride_ptr;
+                    npy_intp count = *innersize_ptr;
+
+                    while(count--) {{
+                        {inp_dtype} {inp_var}_i = *(({inp_dtype}*)data);
+                        {inner_task}
+                        data += stride;
+                    }}
+                }} while(iternext(iter));
+
+                NpyIter_Deallocate(iter);
+                *({acc_dtype}*)(PyArray_DATA({acc_var})) = {acc_var}_i;
+            }}
         }}
         """
+    )
 
-    preloops = {}
-    for i, (loop_order, dtype) in enumerate(zip(loop_orders, dtypes)):
-        for j, index in enumerate(loop_order):
-            if index != "x":
-                preloops.setdefault(j, "")
-                preloops[j] += (
-                    f"%(lv{i})s_iter = ({dtype}*)(PyArray_DATA(%(lv{i})s));\n"
-                ) % sub
-                break
-        else:  # all broadcastable
-            preloops.setdefault(0, "")
-            preloops[0] += (
-                f"%(lv{i})s_iter = ({dtype}*)(PyArray_DATA(%(lv{i})s));\n"
-            ) % sub
 
-    if len(loop_tasks) == 1:
-        s = preloops.get(0, "")
-    else:
-        s = ""
-        for i, (pre_task, task), indices in reversed(
-            list(zip(range(len(loop_tasks) - 1), loop_tasks, list(zip(*loop_orders))))
-        ):
-            s = loop_over(preloops.get(i, "") + pre_task, s + task, indices, i)
+def make_reordered_loop_careduce(
+    inp_var: str,
+    acc_var: str,
+    inp_dtype: str,
+    acc_dtype: str,
+    inp_ndim: int,
+    reduction_axes: Sequence[int],
+    initial_value: str,
+    inner_task: str,
+) -> str:
+    """Generate C code for a partial reduction loop, reordering for optimal memory access of the input variable.
 
-    s += loop_tasks[-1]
-    return f"{{{s}}}"
+    The generated code for a sum along the last axis of a 2D float64 input variable `inp`
+    in an accumulation variable `acc` looks like:
+
+    .. code-block:: C
+        {
+            // Special case for empty inputs
+            if (PyArray_SIZE(inp) == 0) {
+                acc_iter = (npy_float64*)(PyArray_DATA(acc));
+                int_n =  PyArray_SIZE(acc);
+                for(int i = 0; i < n; i++)
+                {
+                    npy_float64 &acc_i = acc_iter[i];
+                    acc_i = 0;
+                }
+            } else {
+            std::vector< std::pair<int, int> > loops(2);
+            std::vector< std::pair<int, int> >::iterator loops_it = loops.begin();
+
+            loops_it->first = abs(PyArray_STRIDES(inp)[0]);
+            loops_it->second = 0;
+            ++loops_it;
+            loops_it->first = abs(PyArray_STRIDES(inp)[1]);
+            loops_it->second = 1;
+            ++loops_it;
+            std::sort(loops.rbegin(), loops.rend());
+
+            int dim_lengths[2] = {inp_n0, inp_n1};
+            int inp_strides[2] = {inp_stride0, inp_stride1};
+            int acc_strides[2] = {acc_stride0, 0};
+            bool reduction_axes[2] = {0, 1};
+
+            loops_it = loops.begin();
+            int dim_length_0 = dim_lengths[loops_it->second];
+            int is_reduction_axis_0 = reduction_axes[loops_it->second];
+            int inp_stride_0 = inp_strides[loops_it->second];
+            int acc_stride_0 = acc_strides[loops_it->second];
+            ++loops_it;
+            int dim_length_1 = dim_lengths[loops_it->second];
+            int is_reduction_axis_1 = reduction_axes[loops_it->second];
+            int inp_stride_1 = inp_strides[loops_it->second];
+            int acc_stride_1 = acc_strides[loops_it->second];
+            ++loops_it;
+
+            inp_iter = (npy_float64*)(PyArray_DATA(inp));
+            acc_iter = (npy_float64*)(PyArray_DATA(acc));
+
+            for(int iter_0 = 0; iter_0<dim_length_0; iter_0++){
+                for(int iter_1 = 0; iter_1<dim_length_1; iter_1++){
+                    npy_float64 &inp_i = *(inp_iter + inp_stride_1*iter_1 + inp_stride_0*iter_0);
+                    npy_float64 &acc_i = *(acc_iter + acc_stride_1*iter_1 + acc_stride_0*iter_0);
+
+                    if((!is_reduction_axis_0 || iter_0 == 0) && (!is_reduction_axis_1 || iter_1 == 0))
+                    {
+                        acc_i = 0;
+                    }
+                    {acc_i = acc_i + inp_i;}
+                }
+            }
+        }
+
+    """
+
+    empty_case = dedent(
+        f"""
+        // Special case for empty inputs
+        if (PyArray_SIZE({inp_var}) == 0) {{
+            {acc_var}_iter = ({acc_dtype}*)(PyArray_DATA({acc_var}));
+            int n =  PyArray_SIZE({acc_var});
+            for(int i = 0; i < n; i++)
+            {{
+                {acc_dtype} &{acc_var}_i = {acc_var}_iter[i];
+                {initial_value}
+            }}
+        }} else {{
+        """
+    )
+
+    # The loops are ordered by (decreasing) absolute values of inp_var's strides.
+    # The first element of each pair is the absolute value of the stride
+    # The second element correspond to the index in the initial loop order
+    order_loops = dedent(
+        f"""
+        std::vector< std::pair<int, int> > loops({inp_ndim});
+        std::vector< std::pair<int, int> >::iterator loops_it = loops.begin();
+        """
+    )
+
+    # Fill the loop vector with the appropriate <stride, index> pairs
+    for i in range(inp_ndim):
+        order_loops += dedent(
+            f"""
+            loops_it->first = abs(PyArray_STRIDES({inp_var})[{i}]);
+            loops_it->second = {i};
+            ++loops_it;"""
+        )
+
+    # We sort in decreasing order so that the outermost loop (loop 0)
+    # has the largest stride, and the innermost loop has the smallest stride.
+    order_loops += "\nstd::sort(loops.rbegin(), loops.rend());\n"
+
+    # Sort shape and strides to match the new order that was computed by sorting the loop vector.
+    counter = iter(range(inp_ndim))
+    unsorted_vars = dedent(
+        f"""
+        int dim_lengths[{inp_ndim}] = {{{','.join(f'{inp_var}_n{i}' for i in range(inp_ndim))}}};
+        int inp_strides[{inp_ndim}] = {{{','.join(f'{inp_var}_stride{i}' for i in range(inp_ndim))}}};
+        int acc_strides[{inp_ndim}] = {{{','.join("0" if i in reduction_axes else f'{acc_var}_stride{next(counter)}'for i in range(inp_ndim))}}};
+        bool reduction_axes[{inp_ndim}] = {{{', '.join("1" if i in reduction_axes else "0" for i in range(inp_ndim))}}};\n
+        """
+    )
+
+    sorted_vars = "loops_it = loops.begin();"
+    for i in range(inp_ndim):
+        sorted_vars += dedent(
+            f"""
+            int dim_length_{i} = dim_lengths[loops_it->second];
+            int is_reduction_axis_{i} = reduction_axes[loops_it->second];
+            int {inp_var}_stride_{i} = inp_strides[loops_it->second];
+            int {acc_var}_stride_{i} = acc_strides[loops_it->second];
+            ++loops_it;
+            """
+        )
+
+    declare_iter = dedent(
+        f"""
+        {inp_var}_iter = ({inp_dtype}*)(PyArray_DATA({inp_var}));
+        {acc_var}_iter = ({acc_dtype}*)(PyArray_DATA({acc_var}));
+        """
+    )
+
+    pointer_update = ""
+    for var, dtype in ((inp_var, inp_dtype), (acc_var, acc_dtype)):
+        pointer_update += f"{dtype} &{var}_i = *({var}_iter"
+        for i in reversed(tuple(range(inp_ndim))):
+            iter_var = f"iter_{i}"
+            pointer_update += f" + {var}_stride_{i}*{iter_var}"
+        pointer_update += ");\n"
+
+    # Set initial value in first iteration of each output
+    # This happens on the first iteration of every reduction axis
+    initial_iteration = " && ".join(
+        f"(!is_reduction_axis_{i} || iter_{i} == 0)" for i in range(inp_ndim)
+    )
+    set_initial_value = dedent(
+        f"""
+        if({initial_iteration})
+        {{
+            {initial_value}
+        }}
+        """
+    )
+
+    # We set do pointer_update, initial_value and inner task in inner loop
+    loop = "\n\n".join((pointer_update, set_initial_value, f"{{{inner_task}}}"))
+
+    # Create outer loops recursively
+    for i in reversed(range(inp_ndim)):
+        iter_var = f"iter_{i}"
+        dim_length = f"dim_length_{i}"
+        loop = dedent(
+            f"""
+            for(int {iter_var} = 0; {iter_var}<{dim_length}; {iter_var}++){{
+                {loop}
+            }}
+            """
+        )
+
+    non_empty_case = "\n".join(
+        (order_loops, unsorted_vars, sorted_vars, declare_iter, loop)
+    )
+    code = "\n".join((empty_case, non_empty_case, "}"))
+    return f"{{\n{code}\n}}\n"
