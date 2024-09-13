0bf00abcd731babbfcba9ab00437d57f996e72fb
Ian Schweer <ischweer@riotgames.com>
2024-09-13 16:31:41 -0700

    Fix linter

--- pytensor/link/pytorch/dispatch/basic.py
+++ pytensor/link/pytorch/dispatch/basic.py
@@ -163,6 +163,7 @@ def pytorch_funcify_IfElse(op, **kwargs):
 
     return ifelse
 
+
 @pytorch_funcify.register(TensorFromScalar)
 def pytorch_funcify_TensorFromScalar(op, **kwargs):
     def tensorfromscalar(x):
