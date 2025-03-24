fix broadcast in multinomial jax dispatch

pytensor/link/jax/dispatch/random.py
@@ -404,11 +404,8 @@ def jax_sample_fn_multinomial(op, node):
 
 def _jax_multinomial(n, p, size=None, key=None):
     if size is not None:
-        broadcast_shape_n = jax.lax.broadcast_shapes(jnp.shape(n), size)
-        n = jnp.broadcast_to(n, broadcast_shape_n)
-
-        broadcast_shape_p = jax.lax.broadcast_shapes(jnp.shape(p)[:-1], size)
-        p = jnp.broadcast_to(p, broadcast_shape_p + jnp.shape(p)[-1:])
+        n = jnp.broadcast_to(n, size)
+        p = jnp.broadcast_to(p, size + jnp.shape(p)[-1:])
 
     else:
         broadcast_shape = jax.lax.broadcast_shapes(jnp.shape(n), jnp.shape(p)[:-1])

tests/link/jax/test_random.py
@@ -722,7 +722,7 @@ def test_multinomial():
     # test with no 'size' argument and n.shape != p.shape[:-1]
     n = np.broadcast_to(np.array([10, 40]), size)
 
-    g = pt.random.multinomial(n, p, rng=rng)
+    g = pt.random.multinomial(n, p, rng=rng, size=None)
     g_fn = compile_random_function([], g, mode="JAX")
     samples = g_fn()
     np.testing.assert_allclose(samples.mean(axis=0), n[0, :, None] * p, rtol=0.1)