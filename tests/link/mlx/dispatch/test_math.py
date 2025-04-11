8a38e2f24554356a55f2e1c50bb1572c98ca0e7a
Will Dean <wd60622@gmail.com>
2025-04-11 11:38:56 -0400

    add test for dot

---
+++ tests/link/mlx/dispatch/test_math.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+import pytensor
+from pytensor.tensor.type import matrix
+
+
+def test_mlx_dot():
+    x = matrix("x")
+    y = matrix("y")
+
+    out = x.dot(y)
+    fn = pytensor.function([x, y], out, mode="MLX")
+
+    test_x = np.random.normal(size=(3, 2))
+    test_y = np.random.normal(size=(2, 4))
+    np.testing.assert_allclose(
+        fn(test_x, test_y),
+        np.dot(test_x, test_y),
+    )
