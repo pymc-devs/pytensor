a9ecad0f8e41ae32e0b0148e2af15695d9c735f7
Will Dean <wd60622@gmail.com>
2025-04-11 12:31:07 -0400

    wrap in mx.array

--- tests/link/mlx/dispatch/test_math.py
+++ tests/link/mlx/dispatch/test_math.py
@@ -1,3 +1,4 @@
+import mlx.core as mx
 import numpy as np
 
 import pytensor
@@ -11,8 +12,8 @@ def test_mlx_dot():
     out = x.dot(y)
     fn = pytensor.function([x, y], out, mode="MLX")
 
-    test_x = np.random.normal(size=(3, 2))
-    test_y = np.random.normal(size=(2, 4))
+    test_x = mx.array(np.random.normal(size=(3, 2)))
+    test_y = mx.array(np.random.normal(size=(2, 4)))
     np.testing.assert_allclose(
         fn(test_x, test_y),
         np.dot(test_x, test_y),
