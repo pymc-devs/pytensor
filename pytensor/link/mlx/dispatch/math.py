7c8eae7aa670d914bb9ad3552f58b4e4bd5279ce
Carlos Trujillo <59846724+cetagostini@users.noreply.github.com>
2025-04-18 13:08:40 -0500

    Adding operations in pytensor

--- pytensor/link/mlx/dispatch/math.py
+++ pytensor/link/mlx/dispatch/math.py
@@ -1,7 +1,10 @@
 import mlx.core as mx
 
 from pytensor.link.mlx.dispatch import mlx_funcify
+
+from pytensor.tensor.elemwise import Elemwise
 from pytensor.tensor.math import Dot
+from pytensor.scalar.basic import Add, Mul, Sub, Exp, Log, Sin, Cos
 
 
 @mlx_funcify.register(Dot)
@@ -10,3 +13,43 @@ def mlx_funcify_Dot(op, **kwargs):
         return mx.matmul(x, y)
 
     return dot
+
+@mlx_funcify.register(Elemwise)
+def mlx_funcify_Elemwise(op, **kwargs):
+    if isinstance(op.scalar_op, Add):
+        def add(x, y):
+            return mx.add(x, y)
+
+        return add
+    elif isinstance(op.scalar_op, Sub):
+        def sub(x, y):
+            return mx.sub(x, y)
+
+        return sub
+    elif isinstance(op.scalar_op, Mul):
+        def mul(x, y):
+            return mx.mul(x, y)
+
+        return mul
+    elif isinstance(op.scalar_op, Exp):
+        def exp(x):
+            return mx.exp(x)
+
+        return exp
+    elif isinstance(op.scalar_op, Log):
+        def log(x):
+            return mx.log(x)
+
+        return log
+    elif isinstance(op.scalar_op, Sin):
+        def sin(x):
+            return mx.sin(x)
+        
+        return sin
+    elif isinstance(op.scalar_op, Cos):
+        def cos(x):
+            return mx.cos(x)
+
+        return cos
+    else:
+        raise NotImplementedError(f"MLX does not support {op.scalar_op}")
