9f67c2c782426fdf4e6ec10677229c539adad5bc
Will Dean <wd60622@gmail.com>
2025-04-18 15:00:45 -0400

    correct the function name

--- pytensor/link/mlx/dispatch/math.py
+++ pytensor/link/mlx/dispatch/math.py
@@ -1,10 +1,9 @@
 import mlx.core as mx
 
 from pytensor.link.mlx.dispatch import mlx_funcify
-
+from pytensor.scalar.basic import Add, Cos, Exp, Log, Mul, Sin, Sub
 from pytensor.tensor.elemwise import Elemwise
 from pytensor.tensor.math import Dot
-from pytensor.scalar.basic import Add, Mul, Sub, Exp, Log, Sin, Cos
 
 
 @mlx_funcify.register(Dot)
@@ -14,39 +13,47 @@ def mlx_funcify_Dot(op, **kwargs):
 
     return dot
 
+
 @mlx_funcify.register(Elemwise)
 def mlx_funcify_Elemwise(op, **kwargs):
     if isinstance(op.scalar_op, Add):
+
         def add(x, y):
             return mx.add(x, y)
 
         return add
     elif isinstance(op.scalar_op, Sub):
+
         def sub(x, y):
-            return mx.sub(x, y)
+            return mx.subtract(x, y)
 
         return sub
     elif isinstance(op.scalar_op, Mul):
+
         def mul(x, y):
-            return mx.mul(x, y)
+            return mx.multiply(x, y)
 
         return mul
     elif isinstance(op.scalar_op, Exp):
+
         def exp(x):
             return mx.exp(x)
 
         return exp
     elif isinstance(op.scalar_op, Log):
+
         def log(x):
             return mx.log(x)
 
         return log
     elif isinstance(op.scalar_op, Sin):
+
         def sin(x):
             return mx.sin(x)
-        
+
         return sin
     elif isinstance(op.scalar_op, Cos):
+
         def cos(x):
             return mx.cos(x)
 
