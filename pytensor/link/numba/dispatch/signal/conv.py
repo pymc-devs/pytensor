9530ffcc22463df63d5d777778a3d3bd85a3c2e8
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2025-04-24 19:06:19 +0200

    Rename core Conv1d to Convolve1d

--- pytensor/link/numba/dispatch/signal/conv.py
+++ pytensor/link/numba/dispatch/signal/conv.py
@@ -2,11 +2,11 @@ import numpy as np
 
 from pytensor.link.numba.dispatch import numba_funcify
 from pytensor.link.numba.dispatch.basic import numba_njit
-from pytensor.tensor.signal.conv import Conv1d
+from pytensor.tensor.signal.conv import Convolve1d
 
 
-@numba_funcify.register(Conv1d)
-def numba_funcify_Conv1d(op, node, **kwargs):
+@numba_funcify.register(Convolve1d)
+def numba_funcify_Convolve1d(op, node, **kwargs):
     mode = op.mode
 
     @numba_njit
