cd51b5f7e2c98366915a2bc74c33bcb8ffb6de49
Jesse Grabowski <jessegrabowski@gmail.com>
2025-07-03 01:14:04 +0800

    Refactor QR

--- pytensor/tensor/slinalg.py
+++ pytensor/tensor/slinalg.py
@@ -7,16 +7,19 @@ from typing import Literal, cast
 import numpy as np
 import scipy.linalg as scipy_linalg
 from numpy.exceptions import ComplexWarning
+from scipy.linalg import get_lapack_funcs
 
 import pytensor
-import pytensor.tensor as pt
+from pytensor import ifelse
+from pytensor import tensor as pt
 from pytensor.gradient import DisconnectedType
 from pytensor.graph.basic import Apply
 from pytensor.graph.op import Op
-from pytensor.tensor import TensorLike, as_tensor_variable
+from pytensor.raise_op import Assert
+from pytensor.tensor import TensorLike
 from pytensor.tensor import basic as ptb
 from pytensor.tensor import math as ptm
-from pytensor.tensor.basic import diagonal
+from pytensor.tensor.basic import as_tensor_variable, diagonal
 from pytensor.tensor.blockwise import Blockwise
 from pytensor.tensor.nlinalg import kron, matrix_dot
 from pytensor.tensor.shape import reshape
@@ -1703,6 +1706,402 @@ def block_diag(*matrices: TensorVariable):
     return _block_diagonal_matrix(*matrices)
 
 
+class QR(Op):
+    """
+    QR Decomposition
+    """
+
+    __props__ = (
+        "overwrite_a",
+        "mode",
+        "lwork_cache",
+        "pivoting",
+        "check_finite",
+        "last_shape_cache",
+    )
+
+    def __init__(
+        self,
+        overwrite_a: bool,
+        mode: Literal["full", "r", "economic", "raw"] = "full",
+        lwork_cache: tuple[int, int] = (-1, -1),
+        pivoting: bool = False,
+        check_finite: bool = False,
+        last_shape_cache: tuple[tuple[int, int], tuple[int, int]] = ((0, 0), (0, 0)),
+    ):
+        self.mode = mode
+        self.overwrite_a = overwrite_a
+        self.lwork_cache = lwork_cache
+        self.pivoting = pivoting
+        self.check_finite = check_finite
+
+        self.destroy_map = {}
+
+        if overwrite_a:
+            self.destroy_map = {0: [0]}
+
+        match self.mode:
+            case "economic":
+                self.gufunc_signature = "(m,n)->(m,k),(k,n)"
+            case "full":
+                self.gufunc_signature = "(m,n)->(m,m),(m,n)"
+            case "r":
+                self.gufunc_signature = "(m,n)->(m,n)"
+            case "raw":
+                self.gufunc_signature = "(m,n)->(n,m),(k),(m,n)"
+            case _:
+                raise ValueError(
+                    f"Invalid mode '{mode}'. Supported modes are 'full', 'economic', 'r', and 'raw'."
+                )
+
+        if pivoting:
+            self.gufunc_signature += ",(n)"
+
+        # Used to cache lwork size in the C backend, where possible
+        # The idea here is that in vectorized cases (e.g. blockwise), the input shape will not change between perform
+        # calls, so we can optimize the LAPACK call to avoid recomputing the optimal lwork size every time.
+        self.last_shape_cache = last_shape_cache
+
+    def make_node(self, x):
+        x = as_tensor_variable(x)
+
+        assert x.ndim == 2, "The input of qr function should be a matrix."
+
+        # Preserve static shape information if possible
+        M, N = x.type.shape
+        if M is not None and N is not None:
+            K = min(M, N)
+        else:
+            K = None
+
+        in_dtype = x.type.numpy_dtype
+        out_dtype = np.dtype(f"f{in_dtype.itemsize}")
+
+        match self.mode:
+            case "full":
+                outputs = [
+                    tensor("Q", shape=(M, M), dtype=out_dtype),
+                    tensor("R", shape=(M, N), dtype=out_dtype),
+                ]
+            case "economic":
+                outputs = [
+                    tensor("Q", shape=(M, K), dtype=out_dtype),
+                    tensor("R", shape=(K, N), dtype=out_dtype),
+                ]
+            case "r":
+                outputs = [
+                    tensor("R", shape=(M, N), dtype=out_dtype),
+                ]
+            case "raw":
+                outputs = [
+                    tensor("H", shape=(M, M), dtype=out_dtype),
+                    tensor("tau", shape=(K,), dtype=out_dtype),
+                    tensor("R", shape=(M, N), dtype=out_dtype),
+                ]
+            case _:
+                raise NotImplementedError
+
+        if self.pivoting:
+            outputs = [*outputs, tensor("P", shape=(N,), dtype="int32")]
+
+        return Apply(self, [x], outputs)
+
+    def infer_shape(self, fgraph, node, shapes):
+        (x_shape,) = shapes
+
+        M, N = x_shape
+        K = ptm.minimum(M, N)
+
+        Q_shape = None
+        R_shape = None
+        tau_shape = None
+        P_shape = None
+
+        match self.mode:
+            case "full":
+                Q_shape = (M, M)
+                R_shape = (M, N)
+            case "economic":
+                Q_shape = (M, K)
+                R_shape = (K, N)
+            case "r":
+                R_shape = (M, N)
+            case "raw":
+                Q_shape = (M, M)  # Actually this is H in this case
+                tau_shape = (K,)
+                R_shape = (M, N)
+
+        if self.pivoting:
+            P_shape = (N,)
+
+        return [
+            shape
+            for shape in (Q_shape, tau_shape, R_shape, P_shape)
+            if shape is not None
+        ]
+
+    def inplace_on_inputs(self, allowed_inplace_inputs: list[int]) -> "Op":
+        if not allowed_inplace_inputs:
+            return self
+        new_props = self._props_dict()  # type: ignore
+        new_props["overwrite_a"] = True
+        return type(self)(**new_props)
+
+    def _call_and_get_lwork(self, fn, func_idx, *args, **kwargs):
+        last_shape_cache = list(self.last_shape_cache)
+        lwork_cache = list(self.lwork_cache)
+
+        last_shape = last_shape_cache[func_idx]
+        lwork = lwork_cache[func_idx]
+
+        M, N = args[0].shape
+
+        if lwork in [-1, None] or last_shape != (M, N):
+            # If we have no lwork cached, or if the shape of the input has changed, we need to recompute lwork.
+            *_, work, info = fn(*args, lwork=-1, **kwargs)
+
+            last_shape_cache[func_idx] = int(work.item())
+            lwork_cache[func_idx] = int(work.item())
+
+            self.last_shape_cache = tuple(last_shape_cache)
+            self.lwork_cache = tuple(lwork_cache)
+
+        return fn(*args, lwork=self.lwork_cache[func_idx], **kwargs)
+
+    def perform(self, node, inputs, outputs):
+        (x,) = inputs
+
+        assert x.ndim == 2, "The input of qr function should be a matrix."
+        M, N = x.shape
+
+        if self.pivoting:
+            (geqp3,) = get_lapack_funcs(("geqp3",), (x,))
+            qr, jpvt, tau, *work_info = self._call_and_get_lwork(
+                geqp3, 0, x, overwrite_a=self.overwrite_a
+            )
+            jpvt -= 1  # geqp3 returns a 1-based index array, so subtract 1
+        else:
+            (geqrf,) = get_lapack_funcs(("geqrf",), (x,))
+            qr, tau, *work_info = self._call_and_get_lwork(
+                geqrf, 0, x, overwrite_a=self.overwrite_a
+            )
+
+        if self.mode not in ["economic", "raw"] or M < N:
+            R = np.triu(qr)
+        else:
+            R = np.triu(qr[:N, :])
+
+        if self.mode == "r" and self.pivoting:
+            outputs[0][0] = R
+            outputs[1][0] = jpvt
+            return
+
+        elif self.mode == "r":
+            outputs[0][0] = R
+            return
+
+        elif self.mode == "raw" and self.pivoting:
+            outputs[0][0] = qr
+            outputs[1][0] = tau
+            outputs[2][0] = R
+            outputs[3][0] = jpvt
+            return
+
+        elif self.mode == "raw":
+            outputs[0][0] = qr
+            outputs[1][0] = tau
+            outputs[2][0] = R
+            return
+
+        (gor_un_gqr,) = get_lapack_funcs(("orgqr",), (qr,))
+
+        if M < N:
+            Q, work, info = self._call_and_get_lwork(
+                gor_un_gqr, 1, qr[:, :M], tau, overwrite_a=1
+            )
+        elif self.mode == "economic":
+            Q, work, info = self._call_and_get_lwork(
+                gor_un_gqr, 1, qr, tau, overwrite_a=1
+            )
+        else:
+            t = qr.dtype.char
+            qqr = np.empty((M, M), dtype=t)
+            qqr[:, :N] = qr
+
+            # Always overwite qqr -- it's a meaningless intermediate value
+            Q, work, info = self._call_and_get_lwork(
+                gor_un_gqr, 1, qqr, tau, overwrite_a=1
+            )
+
+        outputs[0][0] = Q
+        outputs[1][0] = R
+
+        if self.pivoting:
+            outputs[2][0] = jpvt
+
+    def L_op(self, inputs, outputs, output_grads):
+        """
+        Reverse-mode gradient of the QR function.
+
+        References
+        ----------
+        .. [1] Jinguo Liu. "Linear Algebra Autodiff (complex valued)", blog post https://giggleliu.github.io/posts/2019-04-02-einsumbp/
+        .. [2] Hai-Jun Liao, Jin-Guo Liu, Lei Wang, Tao Xiang. "Differentiable Programming Tensor Networks", arXiv:1903.09650v2
+        """
+
+        from pytensor.tensor.slinalg import solve_triangular
+
+        (A,) = (cast(ptb.TensorVariable, x) for x in inputs)
+        m, n = A.shape
+
+        # Check if we have static shape info, if so we can get a better graph (avoiding the ifelse Op in the output)
+        M_static, N_static = A.type.shape
+        shapes_unknown = M_static is None or N_static is None
+
+        def _H(x: ptb.TensorVariable):
+            return x.conj().mT
+
+        def _copyltu(x: ptb.TensorVariable):
+            return ptb.tril(x, k=0) + _H(ptb.tril(x, k=-1))
+
+        if self.mode == "raw":
+            raise NotImplementedError("Gradient of qr not implemented for mode=raw")
+
+        elif self.mode == "r":
+            k = pt.minimum(m, n)
+
+            # We need all the components of the QR to compute the gradient of A even if we only
+            # use the upper triangular component in the cost function.
+            props_dict = self._props_dict()
+            props_dict["mode"] = "economic"
+            props_dict["pivoting"] = False
+
+            qr_op = type(self)(**props_dict)
+
+            Q, R = qr_op(A)
+            dQ = Q.zeros_like()
+
+            # Unlike numpy.linalg.qr, scipy.linalg.qr returns the full (m,n) matrix when mode='r', *not* the (k,n)
+            # matrix that is computed by mode='economic'. The gradient assumes that dR is of shape (k,n), so we need to
+            # slice it to the first k rows. Note that if m <= n, then k = m, so this is safe in all cases.
+            dR = cast(ptb.TensorVariable, output_grads[0][:k, :])
+
+        else:
+            Q, R = (cast(ptb.TensorVariable, x) for x in outputs)
+            if self.mode == "full":
+                qr_assert_op = Assert(
+                    "Gradient of qr not implemented for m x n matrices with m > n and mode=full"
+                )
+                R = qr_assert_op(R, ptm.le(m, n))
+
+            new_output_grads = []
+            is_disconnected = [
+                isinstance(x.type, DisconnectedType) for x in output_grads
+            ]
+            if all(is_disconnected):
+                # This should never be reached by Pytensor
+                return [DisconnectedType()()]  # pragma: no cover
+
+            for disconnected, output_grad, output in zip(
+                is_disconnected, output_grads, [Q, R], strict=True
+            ):
+                if disconnected:
+                    new_output_grads.append(output.zeros_like())
+                else:
+                    new_output_grads.append(output_grad)
+
+            (dQ, dR) = (cast(ptb.TensorVariable, x) for x in new_output_grads)
+
+        if shapes_unknown or M_static >= N_static:
+            # gradient expression when m >= n
+            M = R @ _H(dR) - _H(dQ) @ Q
+            K = dQ + Q @ _copyltu(M)
+            A_bar_m_ge_n = _H(solve_triangular(R, _H(K)))
+
+            if not shapes_unknown:
+                return [A_bar_m_ge_n]
+
+        # We have to trigger both branches if shapes_unknown is True, so this is purposefully not an elif branch
+        if shapes_unknown or M_static < N_static:
+            # gradient expression when m < n
+            Y = A[:, m:]
+            U = R[:, :m]
+            dU, dV = dR[:, :m], dR[:, m:]
+            dQ_Yt_dV = dQ + Y @ _H(dV)
+            M = U @ _H(dU) - _H(dQ_Yt_dV) @ Q
+            X_bar = _H(solve_triangular(U, _H(dQ_Yt_dV + Q @ _copyltu(M))))
+            Y_bar = Q @ dV
+            A_bar_m_lt_n = pt.concatenate([X_bar, Y_bar], axis=1)
+
+            if not shapes_unknown:
+                return [A_bar_m_lt_n]
+
+        return [ifelse(ptm.ge(m, n), A_bar_m_ge_n, A_bar_m_lt_n)]
+
+
+def qr(
+    A: TensorLike,
+    mode: Literal["full", "r", "economic", "raw", "complete", "reduced"] = "full",
+    overwrite_a: bool = False,
+    pivoting: bool = False,
+    lwork: int | None = None,
+):
+    """
+    QR Decomposition of input matrix `a`.
+
+    The QR decomposition of a matrix `A` is a factorization of the form :math`A = QR`, where `Q` is an orthogonal
+    matrix (:math:`Q Q^T = I`) and `R` is an upper triangular matrix.
+
+    This decomposition is useful in various numerical methods, including solving linear systems and least squares
+    problems.
+
+    Parameters
+    ----------
+    A: TensorLike
+        Input matrix of shape (M, N) to be decomposed.
+
+    mode: str, one of "full", "economic", "r", or "raw"
+        How the QR decomposition is computed and returned. Choosing the mode can avoid unnecessary computations,
+        depending on which of the return matrices are needed. Given input matrix with shape  Choices are:
+
+            - "full" (or "complete"): returns `Q` and `R` with dimensions `(M, M)` and `(M, N)`.
+            - "economic" (or "reduced"): returns `Q` and `R` with dimensions `(M, K)` and `(K, N)`,
+                                         where `K = min(M, N)`.
+            - "r": returns only `R` with dimensions `(K, N)`.
+            - "raw": returns `H` and `tau` with dimensions `(N, M)` and `(K,)`, where `H` is the matrix of
+                     Householder reflections, and tau is the vector of Householder coefficients.
+
+    pivoting: bool, default False
+        If True, also return a vector of rank-revealing permutations `P` such that `A[:, P] = QR`.
+
+    overwrite_a: bool, ignored
+        Ignored. Included only for consistency with the function signature of `scipy.linalg.qr`. Pytensor will always
+        automatically overwrite the input matrix `A` if it is safe to do sol.
+
+    lwork: int, ignored
+        Ignored. Included only for consistency with the function signature of `scipy.linalg.qr`. Pytensor will
+        automatically determine the optimal workspace size for the QR decomposition.
+
+    Returns
+    -------
+    Q or H: TensorVariable, optional
+        A matrix with orthonormal columns. When mode = 'complete', it is the result is an orthogonal/unitary matrix
+        depending on whether a is real/complex. The determinant may be either +/- 1 in that case. If
+        mode = 'raw', it is the matrix of Householder reflections. If mode = 'r', Q is not returned.
+
+    R or tau : TensorVariable, optional
+        Upper-triangular matrix. If mode = 'raw', it is the vector of Householder coefficients.
+
+    """
+    # backwards compatibility from the numpy API
+    if mode == "complete":
+        mode = "full"
+    elif mode == "reduced":
+        mode = "economic"
+
+    return Blockwise(QR(mode=mode, pivoting=pivoting, overwrite_a=False))(A)
+
+
 __all__ = [
     "cholesky",
     "solve",
@@ -1717,4 +2116,5 @@ __all__ = [
     "lu",
     "lu_factor",
     "lu_solve",
+    "qr",
 ]
