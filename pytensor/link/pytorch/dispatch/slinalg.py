be949cda9bba642fe876ed7c5a20260f2cbb2604
Jesse Grabowski <jessegrabowski@gmail.com>
2025-07-03 08:43:10 +0800

    Update Torch QR dispatch

---
+++ pytensor/link/pytorch/dispatch/slinalg.py
@@ -0,0 +1,19 @@
+import torch
+
+from pytensor.link.pytorch.dispatch import pytorch_funcify
+from pytensor.tensor.slinalg import QR
+
+
+@pytorch_funcify.register(QR)
+def pytorch_funcify_QR(op, **kwargs):
+    mode = op.mode
+    if mode == "raw":
+        raise NotImplementedError("raw mode not implemented in PyTorch")
+
+    def qr(x):
+        Q, R = torch.linalg.qr(x, mode=mode)
+        if mode == "r":
+            return R
+        return Q, R
+
+    return qr
