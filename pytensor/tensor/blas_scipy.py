9600712bff0475fcc91798ddadb1301764980ca1
ricardoV94 <ricardo.vieira1994@gmail.com>
2025-07-13 19:41:27 +0200

    Remove blas_scipy

--- pytensor/tensor/blas_scipy.py
+++ pytensor/tensor/blas_scipy.py
@@ -2,31 +2,22 @@
 Implementations of BLAS Ops based on scipy's BLAS bindings.
 """
 
+from scipy.linalg.blas import get_blas_funcs
+
 from pytensor.tensor.blas import Ger
 
 
 class ScipyGer(Ger):
     def perform(self, node, inputs, output_storage):
-        from scipy.linalg.blas import get_blas_funcs
-
         cA, calpha, cx, cy = inputs
         (cZ,) = output_storage
-        # N.B. some versions of scipy (e.g. mine) don't actually work
-        # in-place on a, even when I tell it to.
         A = cA
-        local_ger = get_blas_funcs("ger", dtype=cA.dtype)
-        if A.size == 0:
-            # We don't have to compute anything, A is empty.
-            # We need this special case because Numpy considers it
-            # C-contiguous, which is confusing.
-            if not self.destructive:
-                # Sometimes numpy thinks empty matrices can share memory,
-                # so here to stop DebugMode from complaining.
-                A = A.copy()
-        elif A.flags["C_CONTIGUOUS"]:
-            A = local_ger(calpha, cy, cx, a=A.T, overwrite_a=int(self.destructive)).T
+        ger_func = get_blas_funcs("ger", dtype=cA.dtype)
+        if A.flags["C_CONTIGUOUS"]:
+            # Work on transposed system to avoid copying
+            A = ger_func(calpha, cy, cx, a=A.T, overwrite_a=self.destructive).T
         else:
-            A = local_ger(calpha, cx, cy, a=A, overwrite_a=int(self.destructive))
+            A = ger_func(calpha, cx, cy, a=A, overwrite_a=self.destructive)
         cZ[0] = A
 
 
