38719b496ab34d2def144a60b9d29c13f998c987
Will Dean <57733339+williambdean@users.noreply.github.com>
2025-07-28 06:37:54 -0400

    Update test_math.py
    Co-authored-by: Ricardo Vieira <28983449+ricardoV94@users.noreply.github.com>

--- tests/tensor/rewriting/test_math.py
+++ tests/tensor/rewriting/test_math.py
@@ -1993,7 +1993,7 @@ def test_log_sqrt() -> None:
     x = pt.tensor("x", shape=(None, None))
     out = log(sqrt(x))
 
-    out = rewrite_graph(out, include=["canonicalize", "specialize", "stabilize"])
+    out = rewrite_graph(out, include=["canonicalize"])
 
     assert equal_computations(
         [out],
