6c1c9da5d187d87d585a35a9f45f6edd1c2c6f5d
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2025-10-09 17:10:30 +0200

    Move sort Ops dispatchers to their own file

--- tests/link/numba/test_basic.py
+++ tests/link/numba/test_basic.py
@@ -31,7 +31,6 @@ from pytensor.raise_op import assert_op
 from pytensor.scalar.basic import ScalarOp, as_scalar
 from pytensor.tensor import blas, tensor
 from pytensor.tensor.elemwise import Elemwise
-from pytensor.tensor.sort import ArgSortOp, SortOp
 
 
 if TYPE_CHECKING:
@@ -329,70 +328,6 @@ def test_get_numba_type(v, expected, force_scalar, not_implemented):
 def test_create_numba_signature(v, expected, force_scalar):
     res = numba_basic.create_numba_signature(v, force_scalar=force_scalar)
     assert res == expected
-
-
-@pytest.mark.parametrize(
-    "x",
-    [
-        [],  # Empty list
-        [3, 2, 1],  # Simple list
-        np.random.randint(0, 10, (3, 2, 3, 4, 4)),  # Multi-dimensional array
-    ],
-)
-@pytest.mark.parametrize("axis", [0, -1, None])
-@pytest.mark.parametrize(
-    ("kind", "exc"),
-    [
-        ["quicksort", None],
-        ["mergesort", UserWarning],
-        ["heapsort", UserWarning],
-        ["stable", UserWarning],
-    ],
-)
-def test_Sort(x, axis, kind, exc):
-    if axis:
-        g = SortOp(kind)(pt.as_tensor_variable(x), axis)
-    else:
-        g = SortOp(kind)(pt.as_tensor_variable(x))
-
-    cm = contextlib.suppress() if not exc else pytest.warns(exc)
-
-    with cm:
-        compare_numba_and_py([], [g], [])
-
-
-@pytest.mark.parametrize(
-    "x",
-    [
-        [],  # Empty list
-        [3, 2, 1],  # Simple list
-        None,  # Multi-dimensional array (see below)
-    ],
-)
-@pytest.mark.parametrize("axis", [0, -1, None])
-@pytest.mark.parametrize(
-    ("kind", "exc"),
-    [
-        ["quicksort", None],
-        ["heapsort", None],
-        ["stable", UserWarning],
-    ],
-)
-def test_ArgSort(x, axis, kind, exc):
-    if x is None:
-        x = np.arange(5 * 5 * 5 * 5)
-        np.random.shuffle(x)
-        x = np.reshape(x, (5, 5, 5, 5))
-
-    if axis:
-        g = ArgSortOp(kind)(pt.as_tensor_variable(x), axis)
-    else:
-        g = ArgSortOp(kind)(pt.as_tensor_variable(x))
-
-    cm = contextlib.suppress() if not exc else pytest.warns(exc)
-
-    with cm:
-        compare_numba_and_py([], [g], [])
 
 
 def test_ViewOp():
