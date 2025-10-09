80667205c85634ed2749d774d7a09d89fda08e2a
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2025-10-09 17:13:45 +0200

    Move NonZero Op dispatcher to tensor_basic

--- tests/link/numba/test_basic.py
+++ tests/link/numba/test_basic.py
@@ -718,20 +718,6 @@ def test_function_overhead(mode, benchmark):
     benchmark(fn, test_x)
 
 
-@pytest.mark.parametrize(
-    "input_data",
-    [np.array([1, 0, 3]), np.array([[0, 1], [2, 0]]), np.array([[0, 0], [0, 0]])],
-)
-def test_Nonzero(input_data):
-    a = pt.tensor("a", shape=(None,) * input_data.ndim)
-
-    graph_outputs = pt.nonzero(a)
-
-    compare_numba_and_py(
-        graph_inputs=[a], graph_outputs=graph_outputs, test_inputs=[input_data]
-    )
-
-
 @pytest.mark.parametrize("dtype", ("float64", "float32", "mixed"))
 def test_mat_vec_dot_performance(dtype, benchmark):
     A = tensor("A", shape=(512, 512), dtype="float64" if dtype == "mixed" else dtype)
