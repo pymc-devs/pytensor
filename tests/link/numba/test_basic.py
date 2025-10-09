f523fd44576567fc223ac616143e6720d5f81520
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2025-10-09 17:01:50 +0200

    Move shape Ops dispatchers to their own file

--- tests/link/numba/test_basic.py
+++ tests/link/numba/test_basic.py
@@ -31,7 +31,6 @@ from pytensor.raise_op import assert_op
 from pytensor.scalar.basic import ScalarOp, as_scalar
 from pytensor.tensor import blas, tensor
 from pytensor.tensor.elemwise import Elemwise
-from pytensor.tensor.shape import Reshape, Shape, Shape_i, SpecifyShape
 from pytensor.tensor.sort import ArgSortOp, SortOp
 
 
@@ -333,22 +332,6 @@ def test_create_numba_signature(v, expected, force_scalar):
 
 
 @pytest.mark.parametrize(
-    "x, i",
-    [
-        (np.zeros((20, 3)), 1),
-    ],
-)
-def test_Shape(x, i):
-    g = Shape()(pt.as_tensor_variable(x))
-
-    compare_numba_and_py([], [g], [])
-
-    g = Shape_i(i)(pt.as_tensor_variable(x))
-
-    compare_numba_and_py([], [g], [])
-
-
-@pytest.mark.parametrize(
     "x",
     [
         [],  # Empty list
@@ -410,81 +393,6 @@ def test_ArgSort(x, axis, kind, exc):
 
     with cm:
         compare_numba_and_py([], [g], [])
-
-
-@pytest.mark.parametrize(
-    "v, shape, ndim",
-    [
-        ((pt.vector(), np.array([4], dtype=config.floatX)), ((), None), 0),
-        ((pt.vector(), np.arange(4, dtype=config.floatX)), ((2, 2), None), 2),
-        (
-            (pt.vector(), np.arange(4, dtype=config.floatX)),
-            (pt.lvector(), np.array([2, 2], dtype="int64")),
-            2,
-        ),
-    ],
-)
-def test_Reshape(v, shape, ndim):
-    v, v_test_value = v
-    shape, shape_test_value = shape
-
-    g = Reshape(ndim)(v, shape)
-    inputs = [v] if not isinstance(shape, Variable) else [v, shape]
-    test_values = (
-        [v_test_value]
-        if not isinstance(shape, Variable)
-        else [v_test_value, shape_test_value]
-    )
-    compare_numba_and_py(
-        inputs,
-        [g],
-        test_values,
-    )
-
-
-def test_Reshape_scalar():
-    v = pt.vector()
-    v_test_value = np.array([1.0], dtype=config.floatX)
-    g = Reshape(1)(v[0], (1,))
-
-    compare_numba_and_py(
-        [v],
-        g,
-        [v_test_value],
-    )
-
-
-@pytest.mark.parametrize(
-    "v, shape, fails",
-    [
-        (
-            (pt.matrix(), np.array([[1.0]], dtype=config.floatX)),
-            (1, 1),
-            False,
-        ),
-        (
-            (pt.matrix(), np.array([[1.0, 2.0]], dtype=config.floatX)),
-            (1, 1),
-            True,
-        ),
-        (
-            (pt.matrix(), np.array([[1.0, 2.0]], dtype=config.floatX)),
-            (1, None),
-            False,
-        ),
-    ],
-)
-def test_SpecifyShape(v, shape, fails):
-    v, v_test_value = v
-    g = SpecifyShape()(v, *shape)
-    cm = contextlib.suppress() if not fails else pytest.raises(AssertionError)
-
-    with cm:
-        compare_numba_and_py(
-            [v],
-            [g],
-            [v_test_value],
-        )
 
 
 def test_ViewOp():
