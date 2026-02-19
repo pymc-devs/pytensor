2d9a7aeb4a1b6f4b8a203c50703594473741aa31
Arpan Jha <62328473+arpanjha234@users.noreply.github.com>
2026-02-19 18:24:56 +0530

    fix incorrect comment in StructuredDot.grad (#1890)

--- pytensor/sparse/math.py
+++ pytensor/sparse/math.py
@@ -1394,9 +1394,9 @@ class StructuredDot(Op):
         out[0] = np.asarray(variable, str(variable.dtype))
 
     def grad(self, inputs, gout):
-        # FIXME: It's not always true that b and g_out are dense.
-        #        Python implementation (and numba) support sparse 'b' (and thus, 'g_out') as well.
-        # a is sparse, b is dense, g_out is dense
+        # NOTE: a is sparse; b and g_out are both dense or sparse.
+        # Python and Numba backends support sparse b and g_out.
+        # C backend only supports dense b and g_out.
         # ga = g_out x b.T
         # gb = a.T x g_out
         (a, b) = inputs
