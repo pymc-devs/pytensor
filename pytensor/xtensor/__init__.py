82d2c5fb5019fff4db5a8c5fe4d5a3ff3b253c30
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2026-03-17 15:21:37 +0100

    Add xtensor.where as a method and root operation

--- pytensor/xtensor/__init__.py
+++ pytensor/xtensor/__init__.py
@@ -2,7 +2,7 @@ import warnings
 
 import pytensor.xtensor.rewriting
 from pytensor.xtensor import linalg, math, random, signal
-from pytensor.xtensor.math import dot
+from pytensor.xtensor.math import dot, where
 from pytensor.xtensor.shape import broadcast, concat, full_like, ones_like, zeros_like
 from pytensor.xtensor.type import (
     as_xtensor,
