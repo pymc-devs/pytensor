e3e8b8a2da9ccbd028d839c7d28e389fb3d58a2d
jessegrabowski <jessegrabowski@gmail.com>
2026-03-29 00:37:40 -0500

    Add MLX dispatch for Det and SlogDet

--- tests/link/mlx/test_nlinalg.py
+++ tests/link/mlx/test_nlinalg.py
@@ -5,7 +5,30 @@ import pytest
 
 import pytensor.tensor as pt
 from pytensor import config
+from pytensor.compile.mode import get_mode
 from tests.link.mlx.test_basic import compare_mlx_and_py, mlx_mode
+
+
+def test_mlx_det():
+    rng = np.random.default_rng(15)
+
+    A = pt.matrix(name="A")
+    A_val = rng.normal(size=(3, 3)).astype(config.floatX)
+
+    out = pt.linalg.det(A)
+
+    compare_mlx_and_py([A], [out], [A_val])
+
+
+def test_mlx_slogdet():
+    rng = np.random.default_rng(15)
+
+    A = pt.matrix(name="A")
+    A_val = rng.normal(size=(3, 3)).astype(config.floatX)
+
+    sign, logabsdet = pt.linalg.slogdet(A)
+
+    compare_mlx_and_py([A], [sign, logabsdet], [A_val], mlx_mode=get_mode("MLX"))
 
 
 def test_mlx_eig():
