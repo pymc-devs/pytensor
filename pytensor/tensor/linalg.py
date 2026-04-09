45e2b34398079145fc1a2c53010e9301e780cf79
jessegrabowski <jessegrabowski@gmail.com>
2026-04-08 23:35:37 -0500

    Re-import all public functions via `linalg.py`

--- pytensor/tensor/linalg.py
+++ pytensor/tensor/linalg.py
@@ -1,3 +1,133 @@
-from pytensor.tensor._linalg.solve.linear_control import *
-from pytensor.tensor.nlinalg import *
-from pytensor.tensor.slinalg import *
+from pytensor.tensor._linalg.constructors import (  # noqa: F401
+    BaseBlockDiagonal,
+    BlockDiagonal,
+    block_diag,
+)
+from pytensor.tensor._linalg.decomposition.cholesky import (  # noqa: F401
+    Cholesky,
+    cholesky,
+)
+from pytensor.tensor._linalg.decomposition.eigen import (  # noqa: F401
+    Eig,
+    Eigh,
+    EighGrad,
+    Eigvalsh,
+    EigvalshGrad,
+    eig,
+    eigh,
+    eigvalsh,
+)
+from pytensor.tensor._linalg.decomposition.lu import (  # noqa: F401
+    LU,
+    LUFactor,
+    PivotToPermutations,
+    lu,
+    lu_factor,
+    pivot_to_permutation,
+)
+from pytensor.tensor._linalg.decomposition.qr import QR, qr  # noqa: F401
+from pytensor.tensor._linalg.decomposition.schur import (  # noqa: F401
+    QZ,
+    Schur,
+    ordqz,
+    qz,
+    schur,
+)
+from pytensor.tensor._linalg.decomposition.svd import SVD, svd  # noqa: F401
+from pytensor.tensor._linalg.inverse import (  # noqa: F401
+    MatrixInverse,
+    MatrixPinv,
+    TensorInv,
+    inv,
+    matrix_inverse,
+    pinv,
+    tensorinv,
+)
+from pytensor.tensor._linalg.products import (  # noqa: F401
+    Expm,
+    KroneckerProduct,
+    expm,
+    kron,
+    matrix_dot,
+    matrix_power,
+)
+from pytensor.tensor._linalg.solve.core import SolveBase  # noqa: F401
+from pytensor.tensor._linalg.solve.general import (  # noqa: F401
+    Solve,
+    lu_solve,
+    solve,
+)
+from pytensor.tensor._linalg.solve.linear_control import (
+    solve_continuous_lyapunov,
+    solve_discrete_are,
+    solve_discrete_lyapunov,
+    solve_sylvester,
+)
+from pytensor.tensor._linalg.solve.lstsq import (  # noqa: F401
+    Lstsq,
+    TensorSolve,
+    lstsq,
+    tensorsolve,
+)
+from pytensor.tensor._linalg.solve.psd import (  # noqa: F401
+    CholeskySolve,
+    cho_solve,
+)
+from pytensor.tensor._linalg.solve.triangular import (  # noqa: F401
+    SolveTriangular,
+    solve_triangular,
+)
+from pytensor.tensor._linalg.solve.tridiagonal import (  # noqa: F401
+    LUFactorTridiagonal,
+    SolveLUFactorTridiagonal,
+    tridiagonal_lu_factor,
+    tridiagonal_lu_solve,
+)
+from pytensor.tensor._linalg.summary import (  # noqa: F401
+    Det,
+    SLogDet,
+    det,
+    norm,
+    slogdet,
+    trace,
+)
+
+
+__all__ = [
+    "block_diag",
+    "cho_solve",
+    "cholesky",
+    "det",
+    "eig",
+    "eigh",
+    "eigvalsh",
+    "expm",
+    "inv",
+    "kron",
+    "lstsq",
+    "lu",
+    "lu_factor",
+    "lu_solve",
+    "matrix_dot",
+    "matrix_power",
+    "norm",
+    "ordqz",
+    "pinv",
+    "pivot_to_permutation",
+    "qr",
+    "qz",
+    "schur",
+    "slogdet",
+    "solve",
+    "solve_continuous_lyapunov",
+    "solve_discrete_are",
+    "solve_discrete_lyapunov",
+    "solve_sylvester",
+    "solve_triangular",
+    "svd",
+    "tensorinv",
+    "tensorsolve",
+    "trace",
+    "tridiagonal_lu_factor",
+    "tridiagonal_lu_solve",
+]
