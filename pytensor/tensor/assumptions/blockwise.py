1cea19b7fec800d88ff719616500929aeafd8daa
jessegrabowski <jessegrabowski@gmail.com>
2026-04-26 10:18:00 -0400

    Remove duplicate code in blockwise.py

--- pytensor/tensor/assumptions/blockwise.py
+++ pytensor/tensor/assumptions/blockwise.py
@@ -1,29 +1,10 @@
+from pytensor.tensor.assumptions import ALL_KEYS
 from pytensor.tensor.assumptions.core import (
     FactState,
     lookup_assumption_rules,
     register_assumption,
 )
-from pytensor.tensor.assumptions.diagonal import DIAGONAL
-from pytensor.tensor.assumptions.orthogonal import ORTHOGONAL
-from pytensor.tensor.assumptions.positive_definite import (
-    POSITIVE_DEFINITE,
-)
-from pytensor.tensor.assumptions.symmetric import SYMMETRIC
-from pytensor.tensor.assumptions.triangular import (
-    LOWER_TRIANGULAR,
-    UPPER_TRIANGULAR,
-)
 from pytensor.tensor.blockwise import Blockwise
-
-
-ALL_KEYS = (
-    DIAGONAL,
-    LOWER_TRIANGULAR,
-    UPPER_TRIANGULAR,
-    SYMMETRIC,
-    POSITIVE_DEFINITE,
-    ORTHOGONAL,
-)
 
 
 def _blockwise_delegate(op, feature, fgraph, node, input_states):
