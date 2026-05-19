6458accd996bec15126a7a5768f35f29f18e0cff
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2026-05-19 17:26:58 +0200

    Do not clone outer inputs when inlining OFG

--- tests/tensor/rewriting/test_ofg.py
+++ tests/tensor/rewriting/test_ofg.py
@@ -4,6 +4,9 @@ import pytensor
 import pytensor.tensor as pt
 from pytensor import config
 from pytensor.compile.builders import OpFromGraph
+from pytensor.graph.fg import FunctionGraph
+from pytensor.graph.rewriting.basic import dfs_rewriter
+from pytensor.tensor.rewriting.ofg import inline_ofg_expansion
 
 
 @pytest.mark.skipif(
@@ -20,3 +23,18 @@ def test_alloc_diag_inlined():
     nodes = f.maker.fgraph.apply_nodes
 
     assert not any(isinstance(node.op, OpFromGraph) for node in nodes)
+
+
+def test_expansion_no_cloning():
+    x = pt.scalar("x")
+    y = pt.exp(x)
+
+    inner_y = y.type()
+    ofg = OpFromGraph([inner_y], [pt.cos(inner_y)], inline=True)
+    z = ofg(y)
+
+    fg = FunctionGraph(outputs=[y, z])
+    assert len(fg.toposort()) == 2
+
+    dfs_rewriter(inline_ofg_expansion).rewrite(fg)
+    assert len(fg.toposort()) == 2, len(fg.toposort())
