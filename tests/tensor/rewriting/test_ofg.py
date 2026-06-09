c842b592b7c785a21ef646c7e8687fd05d20d59f
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2026-06-09 15:40:20 +0200

    FrozenApply key on output types

--- tests/tensor/rewriting/test_ofg.py
+++ tests/tensor/rewriting/test_ofg.py
@@ -38,3 +38,26 @@ def test_expansion_no_cloning():
 
     dfs_rewriter(inline_ofg_expansion).rewrite(fg)
     assert len(fg.toposort()) == 2, len(fg.toposort())
+
+
+def test_inline_value_dependent_output_shape():
+    """Two inline OpFromGraphs with the same inner structure but different baked
+    output shapes must inline to their own shapes.
+
+    Regression for #2202.
+    """
+
+    x = pt.vector("x", shape=(6,))
+    shp_a = pt.stack([2, 3])
+    shp_b = pt.stack([3, 2])
+    out_a = OpFromGraph([x, shp_a], [pt.reshape(x, shp_a)], inline=True)(x, shp_a)
+    out_b = OpFromGraph([x, shp_b], [pt.reshape(x, shp_b)], inline=True)(x, shp_b)
+    assert out_a.type.shape == (2, 3)
+    assert out_b.type.shape == (3, 2)
+
+    fg = FunctionGraph([x], [out_a, out_b])
+    dfs_rewriter(inline_ofg_expansion).rewrite(fg)
+
+    assert not any(isinstance(n.op, OpFromGraph) for n in fg.apply_nodes)
+    assert fg.outputs[0].type.shape == (2, 3)
+    assert fg.outputs[1].type.shape == (3, 2)
