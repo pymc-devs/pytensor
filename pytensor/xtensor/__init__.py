2ee8919e192118ff2f448c76508f214c05eb3db9
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2026-06-28 12:22:57 +0200

    Move transformer LLM notebook to case studies
    And undo change in stack discoverability

--- pytensor/xtensor/__init__.py
+++ pytensor/xtensor/__init__.py
@@ -1,14 +1,7 @@
 import pytensor.xtensor.rewriting
 from pytensor.xtensor import linalg, math, random, signal
 from pytensor.xtensor.math import dot, where
-from pytensor.xtensor.shape import (
-    broadcast,
-    concat,
-    full_like,
-    ones_like,
-    stack,
-    zeros_like,
-)
+from pytensor.xtensor.shape import broadcast, concat, full_like, ones_like, zeros_like
 from pytensor.xtensor.type import (
     as_xtensor,
     xtensor,
