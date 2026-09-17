4861d5f5a28ea66c75975891184c1fc41bffb362
jessegrabowski <jessegrabowski@gmail.com>
2026-09-17 20:42:49 +0800

    MLX: pass sort axis as a Python int
    Co-authored-by: guillaume-osmo <guillaume@osmo.ai>

--- tests/link/mlx/test_sort.py
+++ tests/link/mlx/test_sort.py
@@ -2,17 +2,26 @@ import numpy as np
 import pytest
 
 from pytensor.tensor.sort import argsort, sort
-from pytensor.tensor.type import matrix
-from tests.link.mlx.test_basic import compare_mlx_and_py
+from pytensor.tensor.type import iscalar, matrix, tensor3
+from tests.link.mlx.test_basic import compare_mlx_and_py, mlx_mode_no_compile
 
 
-@pytest.mark.parametrize("axis", [None, -1])
+@pytest.mark.parametrize("axis", [None, 0, -1, -2])
 @pytest.mark.parametrize("func", (sort, argsort))
 def test_sort(func, axis):
-    x = matrix("x", shape=(2, 2), dtype="float64")
+    x = tensor3("x", shape=(2, 3, 2), dtype="float64")
     out = func(x, axis=axis)
-    arr = np.array([[1.0, 4.0], [5.0, 2.0]])
+    arr = np.random.default_rng(0).permutation(np.arange(12.0)).reshape(2, 3, 2)
     compare_mlx_and_py([x], [out], [arr])
+
+
+@pytest.mark.parametrize("func", (sort, argsort))
+def test_sort_symbolic_axis(func):
+    x = matrix("x", shape=(2, 3), dtype="float64")
+    axis = iscalar("axis")
+    out = func(x, axis=axis)
+    arr = np.random.default_rng(0).permutation(np.arange(6.0)).reshape(2, 3)
+    compare_mlx_and_py([x, axis], [out], [arr, 1], mlx_mode=mlx_mode_no_compile)
 
 
 def test_sort_invalid_kind_warning():
